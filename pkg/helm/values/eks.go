fd934ca59e7965f27e48d4f72c675058b5286f23
Justin Garrison <justinleegarrison@gmail.com>
2022-01-27 21:05:47 +0000

    Add eks-d coredns image to eks templates

--- pkg/helm/values/eks.go
+++ pkg/helm/values/eks.go
@@ -22,6 +22,11 @@ var EKSEtcdVersionMap = map[string]string{
 	"1.20": "public.ecr.aws/eks-distro/etcd-io/etcd:v3.4.15-eks-1-20-10",
 }
 
+var EKSCoreDnsVersionMap = map[string]string{
+	"1.21": "public.ecr.aws/eks-distro/coredns/coredns:v1.8.4-eks-1-21-8",
+	"1.20": "public.ecr.aws/eks-distro/coredns/coredns:v1.8.3-eks-1-20-10",
+}
+
 func getDefaultEKSReleaseValues(chartOptions *helm.ChartOptions, log log.Logger) (string, error) {
 	serverVersionString := GetKubernetesVersion(chartOptions.KubernetesVersion)
 	serverMinorInt, err := GetKubernetesMinorVersion(chartOptions.KubernetesVersion)
@@ -32,17 +37,20 @@ func getDefaultEKSReleaseValues(chartOptions *helm.ChartOptions, log log.Logger)
 	apiImage := EKSAPIVersionMap[serverVersionString]
 	controllerImage := EKSControllerVersionMap[serverVersionString]
 	etcdImage, ok := EKSEtcdVersionMap[serverVersionString]
+	corednsImage, ok := EKSCoreDnsVersionMap[serverVersionString]
 	if !ok {
 		if serverMinorInt > 21 {
 			log.Infof("officially unsupported host server version %s, will fallback to virtual cluster version v1.23", serverVersionString)
 			apiImage = EKSAPIVersionMap["1.21"]
 			controllerImage = EKSControllerVersionMap["1.21"]
 			etcdImage = EKSEtcdVersionMap["1.21"]
+			corednsImage = EKSCoreDnsVersionMap["1.21"]
 		} else {
 			log.Infof("officially unsupported host server version %s, will fallback to virtual cluster version v1.20", serverVersionString)
 			apiImage = EKSAPIVersionMap["1.20"]
 			controllerImage = EKSControllerVersionMap["1.20"]
 			etcdImage = EKSEtcdVersionMap["1.20"]
+			corednsImage = EKSCoreDnsVersionMap["1.20"]
 		}
 	}
 
@@ -57,5 +65,6 @@ etcd:
 	values = strings.ReplaceAll(values, "##API_IMAGE##", apiImage)
 	values = strings.ReplaceAll(values, "##CONTROLLER_IMAGE##", controllerImage)
 	values = strings.ReplaceAll(values, "##ETCD_IMAGE##", etcdImage)
+	values = strings.ReplaceAll(values, "##COREDNS_IMAGE##", corednsImage)
 	return addCommonReleaseValues(values, chartOptions)
 }
