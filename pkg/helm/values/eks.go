a418dd8d63c52d4fbdbd50367ca4a516ef652b44
Justin Garrison <justinleegarrison@gmail.com>
2022-02-01 16:56:19 +0000

    Fix linting errors

--- pkg/helm/values/eks.go
+++ pkg/helm/values/eks.go
@@ -22,7 +22,7 @@ var EKSEtcdVersionMap = map[string]string{
 	"1.20": "public.ecr.aws/eks-distro/etcd-io/etcd:v3.4.15-eks-1-20-10",
 }
 
-var EKSCoreDnsVersionMap = map[string]string{
+var EKSCoreDNSVersionMap = map[string]string{
 	"1.21": "public.ecr.aws/eks-distro/coredns/coredns:v1.8.4-eks-1-21-8",
 	"1.20": "public.ecr.aws/eks-distro/coredns/coredns:v1.8.3-eks-1-20-10",
 }
@@ -36,33 +36,33 @@ func getDefaultEKSReleaseValues(chartOptions *helm.ChartOptions, log log.Logger)
 
 	apiImage := EKSAPIVersionMap[serverVersionString]
 	controllerImage := EKSControllerVersionMap[serverVersionString]
-	etcdImage, ok := EKSEtcdVersionMap[serverVersionString]
-	corednsImage, ok := EKSCoreDnsVersionMap[serverVersionString]
+	etcdImage := EKSEtcdVersionMap[serverVersionString]
+	corednsImage, ok := EKSCoreDNSVersionMap[serverVersionString]
 	if !ok {
 		if serverMinorInt > 21 {
-			log.Infof("officially unsupported host server version %s, will fallback to virtual cluster version v1.23", serverVersionString)
+			log.Infof("officially unsupported host server version %s, will fallback to virtual cluster version v1.21", serverVersionString)
 			apiImage = EKSAPIVersionMap["1.21"]
 			controllerImage = EKSControllerVersionMap["1.21"]
 			etcdImage = EKSEtcdVersionMap["1.21"]
-			corednsImage = EKSCoreDnsVersionMap["1.21"]
+			corednsImage = EKSCoreDNSVersionMap["1.21"]
 		} else {
 			log.Infof("officially unsupported host server version %s, will fallback to virtual cluster version v1.20", serverVersionString)
 			apiImage = EKSAPIVersionMap["1.20"]
 			controllerImage = EKSControllerVersionMap["1.20"]
 			etcdImage = EKSEtcdVersionMap["1.20"]
-			corednsImage = EKSCoreDnsVersionMap["1.20"]
+			corednsImage = EKSCoreDNSVersionMap["1.20"]
 		}
 	}
 
 	// build values
 	values := `api:
-	image: ##API_IMAGE##
-  controller:
-	image: ##CONTROLLER_IMAGE##
-  etcd:
-	image: ##ETCD_IMAGE##
-  coredns:
-	image: ##COREDNS_IMAGE##
+  image: ##API_IMAGE##
+controller:
+  image: ##CONTROLLER_IMAGE##
+etcd:
+  image: ##ETCD_IMAGE##
+coredns:
+  image: ##COREDNS_IMAGE##
 `
 	values = strings.ReplaceAll(values, "##API_IMAGE##", apiImage)
 	values = strings.ReplaceAll(values, "##CONTROLLER_IMAGE##", controllerImage)
