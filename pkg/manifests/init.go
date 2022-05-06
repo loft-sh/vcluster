f22c69a0124d8d571ed1c672eb61220de443fcc4
Ishan Khare <me@ishankhare.dev>
2022-05-06 16:23:00 +0530

    remove dead-code
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

--- pkg/manifests/init.go
+++ pkg/manifests/init.go
@@ -1,53 +1,23 @@
 package manifests
 
 import (
-	"bytes"
 	"context"
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
-	"html/template"
-	"os"
-	"path"
 
 	"github.com/pkg/errors"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
 
-	"github.com/loft-sh/vcluster/pkg/constants"
 	"github.com/loft-sh/vcluster/pkg/util"
-	"github.com/loft-sh/vcluster/pkg/util/applier"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/client-go/rest"
 	"k8s.io/klog"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-const (
-	InitManifestRelativePath = "init/initmanifests.yaml"
-	DEFAULT_NAMESPACE        = "default"
-)
-
 var (
-	LAST_APPLIED_MANIFEST_HASH string
-
 	lastAppliedObjects util.UnstructuredMap
-	ErrorEmptyManifest = errors.New("empty manifest file")
 )
 
 func init() {
 	lastAppliedObjects = make(util.UnstructuredMap)
-}
-
-func ApplyInitManifests(inClusterConfig *rest.Config, namespace string) error {
-	vars := make(map[string]interface{})
-	output, err := processManifestTemplate(vars)
-	if err != nil {
-		return err
-	}
-
-	return applier.ApplyManifest(inClusterConfig,
-		output,
-		applier.WithNamespace(namespace))
 }
 
 func ApplyGivenInitManifests(ctx context.Context, vClient client.Client, defaultNamespace, rawManifests string) error {
@@ -117,87 +87,4 @@ func ApplyGivenInitManifests(ctx context.Context, vClient client.Client, default
 	lastAppliedObjects = currentlyApplyingObjects
 	klog.Infof("successfully applied all init manifest objects")
 	return nil
-}
-
-func processManifestTemplate(vars map[string]interface{}) ([]byte, error) {
-	manifestInputPath := path.Join(constants.ContainerManifestsFolder, InitManifestRelativePath)
-	// check if the file exists, it won't in case init.manifests is null
-	_, err := os.Stat(manifestInputPath)
-	if err != nil {
-		return nil, err
-	}
-
-	ok, err := checkIfEmpty(manifestInputPath)
-	if err != nil {
-		return nil, err
-	}
-	if ok {
-		// file is empty
-		updateLastApplied(manifestInputPath, []byte("---"))
-		return nil, ErrorEmptyManifest
-	}
-
-	manifestTemplate, err := template.ParseFiles(manifestInputPath)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse %s: %v", manifestInputPath, err)
-	}
-
-	buf := new(bytes.Buffer)
-	err = manifestTemplate.Execute(buf, vars)
-	if err != nil {
-		return nil, fmt.Errorf("manifestTemplate.Execute failed for manifest %s: %v", manifestInputPath, err)
-	}
-
-	// update last applied manifest and hash
-	updateLastApplied(manifestInputPath, buf.Bytes())
-
-	return buf.Bytes(), nil
-}
-
-func checkIfEmpty(path string) (bool, error) {
-	contents, err := os.ReadFile(path)
-	if err != nil {
-		return false, err
-	}
-
-	if bytes.Equal(contents, []byte("---")) {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-func ChangeDetected(c client.Client, namespace string) (bool, error) {
-	manifestInputPath := path.Join(constants.ContainerManifestsFolder, InitManifestRelativePath)
-
-	// check if the file exists, it won't in case init.manifests is null
-	_, err := os.Stat(manifestInputPath)
-	if err != nil {
-		return false, err
-	}
-
-	content, err := os.ReadFile(manifestInputPath)
-	if err != nil {
-		return false, err
-	}
-
-	currentHash := hexHasher(content)
-	klog.Info("current", string(content), LAST_APPLIED_MANIFEST_HASH)
-	if currentHash != LAST_APPLIED_MANIFEST_HASH {
-		return true, nil
-	}
-
-	return false, nil
-}
-
-func updateLastApplied(path string, lastAppliedManifest []byte) {
-	content, _ := os.ReadFile(path)
-	LAST_APPLIED_MANIFEST_HASH = hexHasher(content)
-
-	// LAST_APPLIED_MANIFESTS = hex.EncodeToString(lastAppliedManifest[:])
-}
-
-func hexHasher(input []byte) string {
-	hash := md5.Sum(input)
-	return hex.EncodeToString(hash[:])
 }
