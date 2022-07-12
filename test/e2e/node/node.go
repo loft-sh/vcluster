d7ddc1e5ff5f1ffce48fc48e53e56c808369433b
pratikjagrut <26519653+pratikjagrut@users.noreply.github.com>
2022-07-12 13:05:39 +0530

    test: add e2e test for node selector

---
+++ test/e2e/node/node.go
@@ -0,0 +1,30 @@
+package node
+
+import (
+	"github.com/loft-sh/vcluster/test/framework"
+	"github.com/onsi/ginkgo"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = ginkgo.Describe("Node sync", func() {
+	f := framework.DefaultFramework
+	ginkgo.It("sync nodes using label selector", func() {
+		hostNodes, err := f.HostClient.CoreV1().Nodes().List(f.Context, metav1.ListOptions{})
+		framework.ExpectNoError(err)
+
+		virtualNodes, err := f.VclusterClient.CoreV1().Nodes().List(f.Context, metav1.ListOptions{})
+		framework.ExpectNoError(err)
+
+		hostSyncedNodeName := ""
+		hostNodeLabels := make(map[string]map[string]string)
+		for _, node := range hostNodes.Items {
+			hostNodeLabels[node.Name] = node.Labels
+			if node.Labels["kubernetes.io/hostname"] == "kind-control-plane" {
+				hostSyncedNodeName = node.Name
+				break
+			}
+		}
+
+		framework.ExpectEqual(hostSyncedNodeName, virtualNodes.Items[0].Name)
+	})
+})
