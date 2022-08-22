df0c579722909482860612889b653e5c73ead970
Sandeep Kongathi <kn.sandeep@gmail.com>
2022-08-22 11:49:06 +0530

    Updated for Issue - 677
    For issue - https://github.com/loft-sh/vcluster/issues/677
    
    Made a small entry to disconnect file to show message on disconnect

--- cmd/vclusterctl/cmd/disconnect.go
+++ cmd/vclusterctl/cmd/disconnect.go
@@ -75,7 +75,7 @@ func (cmd *DisconnectCmd) Run(cobraCmd *cobra.Command, args []string) error {
 	if err != nil {
 		return errors.Wrap(err, "switch kube context")
 	}
-
+	cmd.log.Infof("Successfully disconnected from vcluster %s", vClusterName)
 	return nil
 }
 
