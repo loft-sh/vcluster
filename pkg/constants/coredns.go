a849acfdd3ffd6b791dc92c37502b3592966541b
Oleg Matskiv <oleg.matskiv@gmail.com>
2022-09-15 10:56:01 +0200

    feat: add support for k3s v1.25.0

--- pkg/constants/coredns.go
+++ pkg/constants/coredns.go
@@ -1,6 +1,7 @@
 package constants
 
 var CoreDNSVersionMap = map[string]string{
+	"1.25": "coredns/coredns:1.9.3",
 	"1.24": "coredns/coredns:1.8.7",
 	"1.23": "coredns/coredns:1.8.6",
 	"1.22": "coredns/coredns:1.8.4",
