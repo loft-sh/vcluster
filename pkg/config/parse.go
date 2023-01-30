852b90147ceef49a0dfb8cc25bb76b0e9574640f
Oleg Matskiv <oleg.matskiv@gmail.com>
2023-01-30 21:59:01 +0100

    feat: additional validation for generic sync config

--- pkg/config/parse.go
+++ pkg/config/parse.go
@@ -3,7 +3,6 @@ package config
 import (
 	"fmt"
 
-	"github.com/pkg/errors"
 	"sigs.k8s.io/yaml"
 )
 
@@ -27,6 +26,11 @@ func validate(config *Config) error {
 		return fmt.Errorf("unsupported configuration version. Only %s is supported by this plugin version", config.Version)
 	}
 
+	err := validateExportDuplicates(config.Exports)
+	if err != nil {
+		return err
+	}
+
 	for idx, exp := range config.Exports {
 		if exp == nil {
 			return fmt.Errorf("exports[%d] is required", idx)
@@ -43,14 +47,47 @@ func validate(config *Config) error {
 		for patchIdx, patch := range exp.Patches {
 			err := validatePatch(patch)
 			if err != nil {
-				return errors.Wrapf(err, "exports[%d].patches[%d]", idx, patchIdx)
+				return fmt.Errorf("invalid exports[%d].patches[%d]: %v", idx, patchIdx, err)
 			}
 		}
 
 		for patchIdx, patch := range exp.ReversePatches {
 			err := validatePatch(patch)
 			if err != nil {
-				return errors.Wrapf(err, "exports[%d].reversPatches[%d]", idx, patchIdx)
+				return fmt.Errorf("invalid exports[%d].reversPatches[%d]: %v", idx, patchIdx, err)
+			}
+		}
+	}
+
+	err = validateImportDuplicates(config.Imports)
+	if err != nil {
+		return err
+	}
+
+	for idx, imp := range config.Imports {
+		if imp == nil {
+			return fmt.Errorf("imports[%d] is required", idx)
+		}
+
+		if imp.Kind == "" {
+			return fmt.Errorf("imports[%d].kind is required", idx)
+		}
+
+		if imp.APIVersion == "" {
+			return fmt.Errorf("imports[%d].APIVersion is required", idx)
+		}
+
+		for patchIdx, patch := range imp.Patches {
+			err := validatePatch(patch)
+			if err != nil {
+				return fmt.Errorf("invalid imports[%d].patches[%d]: %v", idx, patchIdx, err)
+			}
+		}
+
+		for patchIdx, patch := range imp.ReversePatches {
+			err := validatePatch(patch)
+			if err != nil {
+				return fmt.Errorf("invalid imports[%d].reversPatches[%d]: %v", idx, patchIdx, err)
 			}
 		}
 	}
@@ -77,4 +114,32 @@ func validatePatch(patch *Patch) error {
 	default:
 		return fmt.Errorf("unsupported patch type %s", patch.Operation)
 	}
+}
+
+func validateExportDuplicates(exports []*Export) error {
+	gvks := map[string]bool{}
+	for _, e := range exports {
+		k := fmt.Sprintf("%s|%s", e.APIVersion, e.Kind)
+		_, found := gvks[k]
+		if found {
+			return fmt.Errorf("duplicate export for APIVersion %s and %s Kind, only one export for each APIVersion+Kind is permitted", e.APIVersion, e.Kind)
+		}
+		gvks[k] = true
+	}
+
+	return nil
+}
+
+func validateImportDuplicates(imports []*Import) error {
+	gvks := map[string]bool{}
+	for _, e := range imports {
+		k := fmt.Sprintf("%s|%s", e.APIVersion, e.Kind)
+		_, found := gvks[k]
+		if found {
+			return fmt.Errorf("duplicate import for APIVersion %s and %s Kind, only one import for each APIVersion+Kind is permitted", e.APIVersion, e.Kind)
+		}
+		gvks[k] = true
+	}
+
+	return nil
 }
