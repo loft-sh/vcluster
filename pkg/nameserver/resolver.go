1ef31c21cc8ddda01333044ee2e34c09481dc5c2
Ishan Khare <me@ishankhare.dev>
2023-05-31 19:19:01 +0530

    add default nameserver ip finder interface
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

---
+++ pkg/nameserver/resolver.go
@@ -0,0 +1,129 @@
+package nameserver
+
+import (
+	"context"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"k8s.io/apimachinery/pkg/api/equality"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
+)
+
+var Default Interface = DefaultNameserverFinder()
+
+const (
+	DefaultKubeDNSServiceName      = "kube-dns"
+	DefaultKubeDNSServiceNamespace = "kube-system"
+
+	DefaultKubernetesSVCName      = "kubernetes"
+	DefaultKubernetesSVCNamespace = "default"
+)
+
+type ServicePortTranslator func(ports []corev1.ServicePort) []corev1.ServicePort
+
+type SpecialServiceSyncer func(ctx context.Context,
+	vClient,
+	pClient client.Client,
+	svcNamespace,
+	svcName string,
+	vSvcToSync types.NamespacedName,
+	servicePortTranslator ServicePortTranslator) error
+
+type Interface interface {
+	GetDNSServiceName() types.NamespacedName
+	SpecialServicesToSync() map[types.NamespacedName]SpecialServiceSyncer
+}
+
+type NameserverFinder struct {
+	SpecialServices map[types.NamespacedName]SpecialServiceSyncer
+}
+
+func (f *NameserverFinder) GetDNSServiceName() types.NamespacedName {
+	return types.NamespacedName{
+		Name:      DefaultKubeDNSServiceName,
+		Namespace: DefaultKubeDNSServiceNamespace,
+	}
+}
+
+func (f *NameserverFinder) SpecialServicesToSync() map[types.NamespacedName]SpecialServiceSyncer {
+	return f.SpecialServices
+}
+
+func DefaultNameserverFinder() Interface {
+	return &NameserverFinder{
+		SpecialServices: map[types.NamespacedName]SpecialServiceSyncer{
+			{
+				Name:      DefaultKubernetesSVCName,
+				Namespace: DefaultKubernetesSVCNamespace,
+			}: SyncKubernetesService,
+		},
+	}
+}
+
+func SyncKubernetesService(ctx context.Context,
+	vClient,
+	pClient client.Client,
+	svcNamespace,
+	svcName string,
+	vSvcToSync types.NamespacedName,
+	svcPortTranslator ServicePortTranslator) error {
+	// get physical service
+	pObj := &corev1.Service{}
+	err := pClient.Get(ctx, types.NamespacedName{
+		Namespace: svcNamespace,
+		Name:      svcName,
+	}, pObj)
+	if err != nil {
+		if kerrors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+
+	// get virtual service
+	vObj := &corev1.Service{}
+	err = vClient.Get(ctx, vSvcToSync, vObj)
+	if err != nil {
+		if kerrors.IsNotFound(err) {
+			return nil
+		}
+		return err
+	}
+
+	translatedPorts := svcPortTranslator(pObj.Spec.Ports)
+	if vObj.Spec.ClusterIP != pObj.Spec.ClusterIP || !equality.Semantic.DeepEqual(vObj.Spec.Ports, translatedPorts) {
+		newService := vObj.DeepCopy()
+		newService.Spec.ClusterIP = pObj.Spec.ClusterIP
+		newService.Spec.ClusterIPs = pObj.Spec.ClusterIPs
+		newService.Spec.IPFamilies = pObj.Spec.IPFamilies
+		newService.Spec.Ports = translatedPorts
+		if vObj.Spec.ClusterIP != pObj.Spec.ClusterIP || !equality.Semantic.DeepEqual(vObj.Spec.ClusterIPs, pObj.Spec.ClusterIPs) {
+
+			// delete & create with correct ClusterIP
+			err = vClient.Delete(ctx, vObj)
+			if err != nil {
+				return err
+			}
+
+			// make sure we don't set the resource version during create
+			newService.ResourceVersion = ""
+
+			// create the new service with the correct cluster ip
+			err = vClient.Create(ctx, newService)
+			if err != nil {
+				return err
+			}
+		} else {
+			// delete & create with correct ClusterIP
+			err = vClient.Update(ctx, newService)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+
+}
