6c2b281c6842ccf54c492c2b25a8f492ed12dfa3
Thomas Kosiewski <thomas.kosiewski@loft.sh>
2023-06-05 11:21:20 +0200

    Added comments to syncer interface methods
    Signed-off-by: Thomas Kosiewski <thomas.kosiewski@loft.sh>

--- pkg/controllers/syncer/types.go
+++ pkg/controllers/syncer/types.go
@@ -23,11 +23,14 @@ type Syncer interface {
 	Object
 	translator.NameTranslator
 
+	// SyncDown is called when a virtual object was created and needs to be synced down to the physical cluster
 	SyncDown(ctx *synccontext.SyncContext, vObj client.Object) (ctrl.Result, error)
+	// Sync is called to sync a virtual object with a physical object
 	Sync(ctx *synccontext.SyncContext, pObj client.Object, vObj client.Object) (ctrl.Result, error)
 }
 
 type UpSyncer interface {
+	// SyncUp is called when a physical object exists but the virtual object does not exist
 	SyncUp(ctx *synccontext.SyncContext, pObj client.Object) (ctrl.Result, error)
 }
 
