99ff78d1ea2a295c7a986bb812957a71a6c0f80b
Thomas Kosiewski <thomas.kosiewski@loft.sh>
2023-09-21 17:10:48 +0200

    Added reset password sub command
    Signed-off-by: Thomas Kosiewski <thomas.kosiewski@loft.sh>

--- cmd/vclusterctl/cmd/pro/pro.go
+++ cmd/vclusterctl/cmd/pro/pro.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 
 	loftctl "github.com/loft-sh/loftctl/v3/cmd/loftctl/cmd"
+	loftctlreset "github.com/loft-sh/loftctl/v3/cmd/loftctl/cmd/reset"
 	loftctlflags "github.com/loft-sh/loftctl/v3/cmd/loftctl/flags"
 	"github.com/loft-sh/vcluster/cmd/vclusterctl/flags"
 	"github.com/loft-sh/vcluster/cmd/vclusterctl/log"
@@ -28,8 +29,17 @@ func NewProCmd(globalFlags *flags.GlobalFlags) (*cobra.Command, error) {
 	loftctlGlobalFlags := &loftctlflags.GlobalFlags{
 		Silent:    globalFlags.Silent,
 		Debug:     globalFlags.Debug,
-		Config:    globalFlags.Config,
 		LogOutput: globalFlags.LogOutput,
+	}
+
+	if globalFlags.Config != "" {
+		loftctlGlobalFlags.Config = globalFlags.Config
+	} else {
+		var err error
+		loftctlGlobalFlags.Config, err = pro.GetLoftConfigFilePath()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get vcluster pro configuration file path: %w", err)
+		}
 	}
 
 	startCmd, err := NewStartCmd(loftctlGlobalFlags)
@@ -38,6 +48,7 @@ func NewProCmd(globalFlags *flags.GlobalFlags) (*cobra.Command, error) {
 	}
 
 	proCmd.AddCommand(startCmd)
+	proCmd.AddCommand(loftctlreset.NewResetCmd(loftctlGlobalFlags))
 
 	return proCmd, nil
 }
