eb04d584c3bb2aff1382c2e66af1e10727b63af2
Ishan Khare <me@ishankhare.dev>
2023-10-16 18:27:52 +0530

    struct for mapping values (#1282)
    * initial struct for mapping values
    
    * add upto isolation values
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>
    
    * add coredns plugin values
    
    * add unit testing for helm values struct
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>
    
    * use bool pointer for telemetry toggle
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>
    
    * add tests for all distros
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>
    
    * Update cmd/vcluster/cmd/values/v1/base_values.go
    
    Co-authored-by: Thomas Kosiewski <thoma471@googlemail.com>
    
    * fix lint and change plugin config to yaml
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>
    
    * address review comments and improvements
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>
    
    ---------
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>
    Co-authored-by: Thomas Kosiewski <thoma471@googlemail.com>

---
+++ cmd/vcluster/cmd/values/v1/k8s_values.go
@@ -0,0 +1,91 @@
+package values
+
+import corev1 "k8s.io/api/core/v1"
+
+type K8s struct {
+	BaseHelm
+	Syncer     K8sSyncerValues  `json:"syncer,omitempty"`
+	API        APIServerValues  `json:"api,omitempty"`
+	Controller ControllerValues `json:"controller,omitempty"`
+	Scheduler  SchedulerValues  `json:"scheduler,omitempty"`
+	Etcd       EtcdValues       `json:"etcd,omitempty"`
+	Job        JobValues        `json:"job,omitempty"`
+}
+
+type K8sSyncerValues struct {
+	BaseSyncerValues
+	CommonValues
+	ControlPlaneCommonValues
+	SecurityContext    corev1.SecurityContext    `json:"securityContext,omitempty"`
+	PodSecurityContext corev1.PodSecurityContext `json:"podSecurityContext,omitempty"`
+}
+
+type APIServerValues struct {
+	CommonValues
+	SyncerExORCommonValues
+	ControlPlaneCommonValues
+	SecurityContext    corev1.SecurityContext `json:"securityContext,omitempty"`
+	ServiceAnnotations map[string]string      `json:"serviceAnnotations,omitempty"`
+}
+
+type ControllerValues struct {
+	CommonValues
+	SyncerExORCommonValues
+	ControlPlaneCommonValues
+	SecurityContext corev1.SecurityContext `json:"securityContext,omitempty"`
+}
+
+type SchedulerValues struct {
+	CommonValues
+	SyncerExORCommonValues
+	ControlPlaneCommonValues
+}
+
+type EtcdValues struct {
+	CommonValues
+	SyncerExORCommonValues
+	ControlPlaneCommonValues
+	Storage struct {
+		Persistence bool   `json:"persistence,omitempty"`
+		Size        string `json:"size,omitempty"`
+	} `json:"storage,omitempty"`
+	SecurityContext    corev1.SecurityContext `json:"securityContext,omitempty"`
+	ServiceAnnotations map[string]string      `json:"serviceAnnotations,omitempty"`
+}
+
+type JobValues struct {
+	CommonValues
+	SyncerExORCommonValues
+	Enabled         bool `json:"enabled,omitempty"`
+	SecurityContext struct {
+		Capabilities             corev1.Capabilities `json:"capabilities,omitempty"`
+		AllowPrivilegeEscalation bool                `json:"allowPrivilegeEscalation,omitempty"`
+		ReadOnlyRootFilesystem   bool                `json:"readOnlyRootFilesystem,omitempty"`
+		RunAsNonRoot             bool                `json:"runAsNonRoot,omitempty"`
+		RunAsUser                uint32              `json:"runAsUser,omitempty"`
+		RunAsGroup               uint32              `json:"runAsGroup,omitempty"`
+	} `json:"securityContext,omitempty"`
+}
+
+type ControlPlaneCommonValues struct {
+	Image       string            `json:"image,omitempty"`
+	Replicas    uint32            `json:"replicas,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty"`
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+type SyncerExORCommonValues struct {
+	VolumeMounts []corev1.VolumeMount        `json:"volumeMounts,omitempty"`
+	ExtraArgs    []string                    `json:"extraArgs,omitempty"`
+	Resources    corev1.ResourceRequirements `json:"resources,omitempty"`
+}
+
+type CommonValues struct {
+	Volumes           []corev1.Volume     `json:"volumes,omitempty"`
+	PriorityClassName string              `json:"priorityClassName,omitempty"`
+	NodeSelector      corev1.NodeSelector `json:"nodeSelector,omitempty"`
+	Affinity          corev1.Affinity     `json:"affinity,omitempty"`
+	Tolerations       []corev1.Toleration `json:"tolerations,omitempty"`
+	PodAnnotations    map[string]string   `json:"podAnnotations,omitempty"`
+	PodLabels         map[string]string   `json:"podLabels,omitempty"`
+}
---
+++ cmd/vcluster/cmd/values/v1/values_test.go
@@ -0,0 +1,39 @@
+package values
+
+import (
+	"os"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/util/yaml"
+)
+
+var sourceFiles = map[string]string{
+	"k3s": "../../../../../charts/k3s/values.yaml",
+	"k8s": "../../../../../charts/k8s/values.yaml",
+	"eks": "../../../../../charts/eks/values.yaml",
+	"k0s": "../../../../../charts/k0s/values.yaml",
+}
+
+func TestHelmValues(t *testing.T) {
+	for k, v := range sourceFiles {
+		t.Logf("testing values for %s", k)
+		sourceValuesRaw, err := os.ReadFile(v)
+		if err != nil {
+			t.Fatalf("error reading source values files %v", err)
+		}
+
+		var helmValues interface{}
+
+		switch k {
+		case "k3s", "k0s":
+			helmValues = &K3s{}
+		case "k8s", "eks":
+			helmValues = &K8s{}
+		}
+
+		err = yaml.UnmarshalStrict(sourceValuesRaw, helmValues)
+		if err != nil {
+			t.Fatalf("error unmarshalling raw values %s", err)
+		}
+	}
+}
