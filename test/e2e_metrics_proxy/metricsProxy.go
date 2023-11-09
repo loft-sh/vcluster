05b917e1730b2b365bdc51e282e8aff5d3b4e1b8
Ishan Khare <me@ishankhare.dev>
2023-11-09 12:36:07 +0530

    add tests for metrics proxy - node and pod metrics
    enable hostNetwork for metrics server
    
    Signed-off-by: Ishan Khare <me@ishankhare.dev>

--- test/e2e_metrics_proxy/metricsProxy.go
+++ test/e2e_metrics_proxy/metricsProxy.go
@@ -2,6 +2,7 @@ package e2emetricsproxy
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/loft-sh/vcluster/pkg/metricsapiservice"
@@ -12,13 +13,15 @@ import (
 	"k8s.io/apimachinery/pkg/util/wait"
 	apiregistrationv1 "k8s.io/kube-aggregator/pkg/apis/apiregistration/v1"
 	apiregistrationv1clientset "k8s.io/kube-aggregator/pkg/client/clientset_generated/clientset/typed/apiregistration/v1"
+
+	metricsv1beta1client "k8s.io/metrics/pkg/client/clientset/versioned/typed/metrics/v1beta1"
 )
 
 var _ = ginkgo.Describe("Target Namespace", func() {
 	f := framework.DefaultFramework
 
 	ginkgo.It("Make sure the metrics api service is registered and available", func() {
-		err := wait.PollUntilContextTimeout(f.Context, time.Second, time.Minute*1, false, func(ctx context.Context) (done bool, err error) {
+		err := wait.PollUntilContextTimeout(f.Context, time.Second, time.Minute*1, false, func(ctx context.Context) (bool, error) {
 			apiRegistrationClient := apiregistrationv1clientset.NewForConfigOrDie(f.VclusterConfig)
 			apiService, err := apiRegistrationClient.APIServices().Get(f.Context, metricsapiservice.MetricsAPIServiceName, metav1.GetOptions{})
 			if err != nil {
@@ -31,6 +34,38 @@ var _ = ginkgo.Describe("Target Namespace", func() {
 
 			return true, nil
 		})
+		framework.ExpectNoError(err)
+	})
+
+	ginkgo.It("Make sure get nodeMetrics and podMetrics succeed", func() {
+		err := wait.PollUntilContextTimeout(f.Context, time.Second, time.Minute*1, false, func(ctx context.Context) (bool, error) {
+			metricsClient := metricsv1beta1client.NewForConfigOrDie(f.VclusterConfig)
+
+			nodeMetricsList, err := metricsClient.NodeMetricses().List(f.Context, metav1.ListOptions{})
+			if err != nil {
+				fmt.Fprintf(ginkgo.GinkgoWriter, "error getting node metrics list %v", err)
+				return false, nil
+			}
+
+			if len(nodeMetricsList.Items) == 0 {
+				fmt.Fprintf(ginkgo.GinkgoWriter, "expecting node metrics list to have at least 1 entry, got %d", len(nodeMetricsList.Items))
+				return false, nil
+			}
+
+			podMetricsList, err := metricsClient.PodMetricses("kube-system").List(f.Context, metav1.ListOptions{})
+			if err != nil {
+				fmt.Fprintf(ginkgo.GinkgoWriter, "error getting pod metrics list %v", err)
+				return false, nil
+			}
+
+			if len(podMetricsList.Items) == 0 {
+				fmt.Fprintf(ginkgo.GinkgoWriter, "expecting pod metrics list to have at least 1 entry, got %d", len(podMetricsList.Items))
+				return false, nil
+			}
+
+			return true, nil
+		})
+
 		framework.ExpectNoError(err)
 	})
 })
