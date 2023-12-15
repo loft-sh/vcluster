9c1f8e5a25d7b353b935fb1a66cb5e3f646d1bf1
facchettos <facchettos@gmail.com>
2023-12-15 09:36:43 +0100

    added comment

--- pkg/util/servicecidr/servicecidr.go
+++ pkg/util/servicecidr/servicecidr.go
@@ -116,6 +116,7 @@ func EnsureServiceCIDRInK0sSecret(
 	if warning != "" {
 		klog.Info(warning)
 	}
+	// only return the first cidr, because k0s don't accept coma separated ones
 	serviceCIDR = strings.Split(serviceCIDR, ",")[0]
 
 	// apply changes
