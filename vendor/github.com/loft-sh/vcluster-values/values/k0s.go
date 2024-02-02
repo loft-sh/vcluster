5ce4ce8256343edb1a86628905e62c6b64f09c09
facchettos <facchettos@gmail.com>
2024-02-02 14:59:25 +0100

    removed kubelet-config as it is removed in k0s

--- vendor/github.com/loft-sh/vcluster-values/values/k0s.go
+++ vendor/github.com/loft-sh/vcluster-values/values/k0s.go
@@ -7,10 +7,10 @@ import (
 )
 
 var K0SVersionMap = map[string]string{
+	"1.29": "k0sproject/k0s:v1.29.1-k0s.0",
 	"1.28": "k0sproject/k0s:v1.28.2-k0s.0",
 	"1.27": "k0sproject/k0s:v1.27.6-k0s.0",
 	"1.26": "k0sproject/k0s:v1.26.9-k0s.0",
-	"1.25": "k0sproject/k0s:v1.25.14-k0s.0",
 }
 
 func getDefaultK0SReleaseValues(chartOptions *ChartOptions, log logr.Logger) (string, error) {
@@ -25,12 +25,12 @@ func getDefaultK0SReleaseValues(chartOptions *ChartOptions, log logr.Logger) (st
 		var ok bool
 		image, ok = K0SVersionMap[serverVersionString]
 		if !ok {
-			if serverMinorInt > 28 {
-				log.Info("officially unsupported host server version, will fallback to virtual cluster version v1.28", "serverVersion", serverVersionString)
-				image = K0SVersionMap["1.28"]
+			if serverMinorInt > 29 {
+				log.Info("officially unsupported host server version, will fallback to virtual cluster version v1.29", "serverVersion", serverVersionString)
+				image = K0SVersionMap["1.29"]
 			} else {
-				log.Info("officially unsupported host server version, will fallback to virtual cluster version v1.25", "serverVersion", serverVersionString)
-				image = K0SVersionMap["1.25"]
+				log.Info("officially unsupported host server version, will fallback to virtual cluster version v1.26", "serverVersion", serverVersionString)
+				image = K0SVersionMap["1.26"]
 			}
 		}
 	}
