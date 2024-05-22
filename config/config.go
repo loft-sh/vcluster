aac1e31b9966c5a6178423bb662d0c592b920594
Ryan Swanson <ryan.a.swanson@gmail.com>
2024-05-22 07:37:41 -0600

    Adds enableServiceLinks to the STS config for controlPlane and controlPlane.backingStore (#1789)
    * Adds enableServiceLinks to the STS config for controlPlane and controlPlane.backingStore
    
    * Updates comment to be clearer

--- config/config.go
+++ config/config.go
@@ -620,6 +620,9 @@ type ControlPlaneStatefulSet struct {
 	// Persistence defines options around persistence for the statefulSet.
 	Persistence ControlPlanePersistence `json:"persistence,omitempty"`
 
+	// EnableServiceLinks for the StatefulSet pod
+	EnableServiceLinks *bool `json:"enableServiceLinks,omitempty"`
+
 	LabelsAndAnnotations `json:",inline"`
 
 	// Additional labels or annotations for the statefulSet pods.
@@ -879,6 +882,9 @@ type EtcdDeployHeadlessService struct {
 type EtcdDeployStatefulSet struct {
 	// Enabled defines if the statefulSet should be deployed
 	Enabled bool `json:"enabled,omitempty"`
+
+	// EnableServiceLinks for the StatefulSet pod
+	EnableServiceLinks *bool `json:"enableServiceLinks,omitempty"`
 
 	// Image is the image to use for the external etcd statefulSet
 	Image Image `json:"image,omitempty"`
