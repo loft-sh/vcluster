1e604fcc51e71367d014c1240b75546347620795
facchettos <facchettos@gmail.com>
2024-05-28 16:28:45 +0200

    removed dup consts

--- cmd/vclusterctl/cmd/platform/get/get.go
+++ cmd/vclusterctl/cmd/platform/get/get.go
@@ -8,12 +8,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	OutputYAML  string = "yaml"
-	OutputJSON  string = "json"
-	OutputValue string = "value"
-)
-
 // NewGetCmd creates a new cobra command for the sub command
 func NewGetCmd(globalFlags *flags.GlobalFlags, defaults *defaults.Defaults, cfg *config.CLI) *cobra.Command {
 	description := product.ReplaceWithHeader("var", "")
