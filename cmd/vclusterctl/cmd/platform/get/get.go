1553566e66a08e8352155f587034c193ffa4a0fd
Jeremy Facchetti <facchettos@gmail.com>
2024-05-29 10:40:43 +0200

    Cluster access key (#1804)

--- cmd/vclusterctl/cmd/platform/get/get.go
+++ cmd/vclusterctl/cmd/platform/get/get.go
@@ -8,6 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// NewGetCmd creates a new cobra command for the sub command
 func NewGetCmd(globalFlags *flags.GlobalFlags, defaults *defaults.Defaults, cfg *config.CLI) *cobra.Command {
 	description := product.ReplaceWithHeader("var", "")
 
@@ -19,6 +20,7 @@ func NewGetCmd(globalFlags *flags.GlobalFlags, defaults *defaults.Defaults, cfg 
 	}
 
 	cmd.AddCommand(newClusterCmd(globalFlags, cfg))
+	cmd.AddCommand(newClusterAccessKeyCmd(globalFlags, cfg))
 	cmd.AddCommand(newSecretCmd(globalFlags, defaults, cfg))
 	return cmd
 }
