68565217fbae3ca56bbfe1b379b2d4979b147120
Jeremy Facchetti <facchettos@gmail.com>
2024-07-15 18:33:46 +0200

    added logic with patcher for ingressclasses (#1931)
    * added logic with patcher
    
    * rename
    
    * simplified logic

--- pkg/controllers/resources/ingressclasses/syncer.go
+++ pkg/controllers/resources/ingressclasses/syncer.go
@@ -1,10 +1,14 @@
 package ingressclasses
 
 import (
+	"fmt"
+
 	synccontext "github.com/loft-sh/vcluster/pkg/controllers/syncer/context"
 	"github.com/loft-sh/vcluster/pkg/controllers/syncer/translator"
+	"github.com/loft-sh/vcluster/pkg/patcher"
 	syncer "github.com/loft-sh/vcluster/pkg/types"
 	networkingv1 "k8s.io/api/networking/v1"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -23,19 +27,27 @@ var _ syncer.ToVirtualSyncer = &ingressClassSyncer{}
 var _ syncer.Syncer = &ingressClassSyncer{}
 
 func (i *ingressClassSyncer) SyncToVirtual(ctx *synccontext.SyncContext, pObj client.Object) (ctrl.Result, error) {
-	vObj := i.translateBackwards(ctx.Context, pObj.(*networkingv1.IngressClass))
+	vObj := i.createVirtual(ctx.Context, pObj.(*networkingv1.IngressClass))
 	ctx.Log.Infof("create ingress class %s, because it does not exist in virtual cluster", vObj.Name)
 	return ctrl.Result{}, ctx.VirtualClient.Create(ctx.Context, vObj)
 }
 
-func (i *ingressClassSyncer) Sync(ctx *synccontext.SyncContext, pObj, vObj client.Object) (ctrl.Result, error) {
-	updated := i.translateUpdateBackwards(ctx.Context, pObj.(*networkingv1.IngressClass), vObj.(*networkingv1.IngressClass))
-	if updated != nil {
-		ctx.Log.Infof("update ingress class %s", vObj.GetName())
-		translator.PrintChanges(pObj, updated, ctx.Log)
-		return ctrl.Result{}, ctx.VirtualClient.Update(ctx.Context, updated)
+func (i *ingressClassSyncer) Sync(ctx *synccontext.SyncContext, pObj, vObj client.Object) (_ ctrl.Result, retErr error) {
+	patch, err := patcher.NewSyncerPatcher(ctx, pObj, vObj)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("new syncer patcher: %w", err)
 	}
 
+	defer func() {
+		if err := patch.Patch(ctx, pObj, vObj); err != nil {
+			retErr = utilerrors.NewAggregate([]error{retErr, err})
+		}
+	}()
+
+	// cast objects
+	pIngressClass, vIngressClass, _, _ := synccontext.Cast[*networkingv1.IngressClass](ctx, pObj, vObj)
+
+	i.updateVirtual(ctx.Context, pIngressClass, vIngressClass)
 	return ctrl.Result{}, nil
 }
 
