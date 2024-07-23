9b5dcb55991928f914ffe68ed2837eba8ce6ec65
facchettos <facchettos@gmail.com>
2024-07-23 10:42:09 +0200

    refactored the remaining tests

--- pkg/controllers/resources/nodes/syncer_test.go
+++ pkg/controllers/resources/nodes/syncer_test.go
@@ -3,13 +3,11 @@ package nodes
 import (
 	"testing"
 
-	"github.com/loft-sh/vcluster/pkg/syncer"
 	"github.com/loft-sh/vcluster/pkg/syncer/synccontext"
 	syncertesting "github.com/loft-sh/vcluster/pkg/syncer/testing"
 	syncertypes "github.com/loft-sh/vcluster/pkg/syncer/types"
 	"github.com/loft-sh/vcluster/pkg/util/translate"
 	"gotest.tools/assert"
-	controllerruntime "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/loft-sh/vcluster/pkg/constants"
@@ -154,6 +152,12 @@ func TestSyncBothExist(t *testing.T) {
 		hostLabel               map[string]string
 		virtualFinalLabels      map[string]string
 		virtualFinalAnnotations map[string]string
+		physicalAnnotations     map[string]string
+		imagesPhysicalNode      []corev1.ContainerImage
+		enforceTolerations      []string
+		clearImage              bool
+		physicalTaints          []corev1.Taint
+		expectedTaints          []corev1.Taint
 	}{
 		{
 			name:              "Update backward no change",
@@ -180,6 +184,90 @@ func TestSyncBothExist(t *testing.T) {
 			virtualFinalAnnotations: map[string]string{translate.ManagedLabelsAnnotation: "test"},
 			virtualFinalLabels:      map[string]string{"test": "true"},
 		},
+		{
+			name:              "Label Not Matched and enforceNodeSelector true - expect node not to be synced",
+			virtualNodeExists: true,
+			syncFromHostLabel: map[string]string{"test": "true"},
+			expectNoVNode:     true,
+		},
+		{
+			name:               "Clear Nodes",
+			virtualNodeExists:  true,
+			withVirtualPod:     true,
+			clearImage:         true,
+			imagesPhysicalNode: []corev1.ContainerImage{{Names: []string{"ghcr.io/jetpack/calico"}}},
+		},
+		{
+			name:               "Don't Clear Nodes",
+			virtualNodeExists:  true,
+			withVirtualPod:     true,
+			clearImage:         false,
+			imagesPhysicalNode: []corev1.ContainerImage{{Names: []string{"ghcr.io/jetpack/calico"}}},
+		},
+		{
+			name: "Matching taints",
+			physicalAnnotations: map[string]string{
+				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
+			},
+			virtualFinalAnnotations: map[string]string{
+				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
+			},
+			virtualNodeExists:  true,
+			withVirtualPod:     true,
+			enforceTolerations: []string{":NoSchedule op=Exists"},
+			physicalTaints: []corev1.Taint{
+				{
+					Key:    "key1",
+					Value:  "value1",
+					Effect: "NoSchedule",
+				},
+			},
+		},
+		{
+			name: "Not Matching taints",
+			physicalAnnotations: map[string]string{
+				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
+			},
+			virtualFinalAnnotations: map[string]string{
+				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
+			},
+			virtualNodeExists:  true,
+			withVirtualPod:     true,
+			enforceTolerations: []string{"key2=value2:NoSchedule"},
+			physicalTaints: []corev1.Taint{
+				{
+					Key:    "key1",
+					Value:  "value1",
+					Effect: "NoSchedule",
+				},
+			},
+			expectedTaints: []corev1.Taint{
+				{
+					Key:    "key1",
+					Value:  "value1",
+					Effect: "NoSchedule",
+				},
+			},
+		},
+		{
+			name: "Taint matching Enforced Toleration - special case of empty key with Exists operator",
+			physicalAnnotations: map[string]string{
+				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
+			},
+			virtualFinalAnnotations: map[string]string{
+				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
+			},
+			virtualNodeExists:  true,
+			withVirtualPod:     true,
+			enforceTolerations: []string{":NoSchedule op=Exists"},
+			physicalTaints: []corev1.Taint{
+				{
+					Key:    "key1",
+					Value:  "value1",
+					Effect: "NoSchedule",
+				},
+			},
+		},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.name, func(t *testing.T) {
@@ -205,12 +293,18 @@ func TestSyncBothExist(t *testing.T) {
 			}
 
 			physical.Labels = tC.hostLabel
+			physical.Status.Images = tC.imagesPhysicalNode
+			if !tC.clearImage && tC.imagesPhysicalNode != nil {
+				expectedVNode.Status.Images = tC.imagesPhysicalNode
+			}
+			physical.Spec.Taints = tC.physicalTaints
 
 			if !tC.expectNoVNode {
 				expectedVirtualObjects[corev1.SchemeGroupVersion.WithKind("Node")] = []runtime.Object{expectedVNode}
 				expectedVNode.Labels = tC.virtualFinalLabels
 				expectedVNode.Annotations = tC.virtualFinalAnnotations
 			}
+			expectedVNode.Spec.Taints = tC.expectedTaints
 
 			if tC.modifiedPhysical {
 				expectedVirtualObjects[corev1.SchemeGroupVersion.WithKind("Node")] = []runtime.Object{editedNode.DeepCopy()}
@@ -226,8 +320,10 @@ func TestSyncBothExist(t *testing.T) {
 			// setting up the clients
 			pClient, vClient, vConfig := test.Setup()
 			registerContext := syncertesting.NewFakeRegisterContext(vConfig, pClient, vClient)
-			registerContext.Config.Networking.Advanced.ProxyKubelets.ByIP = false
 
+			registerContext.Config.Networking.Advanced.ProxyKubelets.ByIP = false
+			registerContext.Config.Sync.FromHost.Nodes.ClearImageStatus = tC.clearImage
+			registerContext.Config.Sync.ToHost.Pods.EnforceTolerations = tC.enforceTolerations
 			registerContext.Config.Sync.FromHost.Nodes.Selector.Labels = tC.syncFromHostLabel
 
 			syncCtx, syncer := newFakeSyncer(t, registerContext)
@@ -251,419 +347,4 @@ func newFakeSyncer(t *testing.T, ctx *synccontext.RegisterContext) (*synccontext
 		return NewSyncer(ctx, &fakeNodeServiceProvider{})
 	})
 	return syncContext, object.(*nodeSyncer)
-}
-
-func TestNodeDeletion(t *testing.T) {
-	baseName := types.NamespacedName{
-		Name: "mynode",
-	}
-	baseNode := &corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-		},
-		Status: corev1.NodeStatus{
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: 0,
-				},
-			},
-		},
-	}
-
-	syncertesting.RunTests(t, []*syncertesting.SyncTest{
-		{
-			Name:                 "Delete unused node backwards",
-			InitialVirtualState:  []runtime.Object{baseNode},
-			InitialPhysicalState: []runtime.Object{baseNode},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{
-				corev1.SchemeGroupVersion.WithKind("Node"): {},
-			},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				_, nodesSyncer := newFakeSyncer(t, ctx)
-				syncController, err := syncer.NewSyncController(ctx, nodesSyncer)
-				assert.NilError(t, err)
-
-				_, err = syncController.Reconcile(ctx, controllerruntime.Request{NamespacedName: baseName})
-				assert.NilError(t, err)
-			},
-		},
-	})
-}
-
-func TestTaints(t *testing.T) {
-	baseNode := &corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-			Annotations: map[string]string{
-				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
-			},
-		},
-		Spec: corev1.NodeSpec{
-			Taints: []corev1.Taint{
-				{
-					Key:    "key1",
-					Value:  "value1",
-					Effect: "NoSchedule",
-				},
-			},
-		},
-		Status: corev1.NodeStatus{
-			Addresses: []corev1.NodeAddress{
-				{
-					Address: GetNodeHost(baseName.Name),
-					Type:    corev1.NodeHostName,
-				},
-			},
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: constants.KubeletPort,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-		},
-	}
-
-	editedNode := &corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-			Annotations: map[string]string{
-				TaintsAnnotation: "[\"{\\\"key\\\":\\\"key1\\\",\\\"value\\\":\\\"value1\\\",\\\"effect\\\":\\\"NoSchedule\\\"}\"]",
-			},
-		},
-		Spec: corev1.NodeSpec{
-			Taints: nil,
-		},
-		Status: corev1.NodeStatus{
-			Addresses: []corev1.NodeAddress{
-				{
-					Address: GetNodeHost(baseName.Name),
-					Type:    corev1.NodeHostName,
-				},
-			},
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: constants.KubeletPort,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-		},
-	}
-
-	syncertesting.RunTests(t, []*syncertesting.SyncTest{
-		{
-			Name:                 "Taint matching Enforced Toleration",
-			InitialPhysicalState: []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			InitialVirtualState:  []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{
-				corev1.SchemeGroupVersion.WithKind("Node"): {editedNode.DeepCopy()},
-				corev1.SchemeGroupVersion.WithKind("Pod"):  {basePod.DeepCopy()},
-			},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				ctx.Config.Sync.ToHost.Pods.EnforceTolerations = []string{"key1=value1:NoSchedule"}
-				ctx.Config.Networking.Advanced.ProxyKubelets.ByIP = false
-				syncCtx, syncer := newFakeSyncer(t, ctx)
-				_, err := syncer.Sync(syncCtx, baseNode.DeepCopy(), baseNode.DeepCopy())
-				assert.NilError(t, err)
-			},
-		},
-		{
-			Name:                 "Taint not matching Enforced Toleration",
-			InitialPhysicalState: []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			InitialVirtualState:  []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{
-				corev1.SchemeGroupVersion.WithKind("Node"): {baseNode.DeepCopy()},
-				corev1.SchemeGroupVersion.WithKind("Pod"):  {basePod.DeepCopy()},
-			},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				ctx.Config.Sync.ToHost.Pods.EnforceTolerations = []string{"key2=value2:NoSchedule"}
-				ctx.Config.Networking.Advanced.ProxyKubelets.ByIP = false
-				syncCtx, syncer := newFakeSyncer(t, ctx)
-				_, err := syncer.Sync(syncCtx, baseNode.DeepCopy(), baseNode.DeepCopy())
-				assert.NilError(t, err)
-			},
-		},
-		{
-			Name:                 "Taint matching Enforced Toleration - special case of empty key with Exists operator",
-			InitialPhysicalState: []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			InitialVirtualState:  []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{
-				corev1.SchemeGroupVersion.WithKind("Node"): {editedNode.DeepCopy()},
-				corev1.SchemeGroupVersion.WithKind("Pod"):  {basePod.DeepCopy()},
-			},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				ctx.Config.Sync.ToHost.Pods.EnforceTolerations = []string{":NoSchedule op=Exists"}
-				ctx.Config.Networking.Advanced.ProxyKubelets.ByIP = false
-				syncCtx, syncer := newFakeSyncer(t, ctx)
-				_, err := syncer.Sync(syncCtx, baseNode.DeepCopy(), baseNode.DeepCopy())
-				assert.NilError(t, err)
-			},
-		},
-	})
-}
-
-func TestLabelSelector(t *testing.T) {
-	baseName = types.NamespacedName{
-		Name: "mynode",
-	}
-	basePod = corev1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "mypod",
-		},
-		Spec: corev1.PodSpec{
-			NodeName: baseName.Name,
-		},
-	}
-	baseNode = corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-			Labels: map[string]string{
-				"test": "true",
-			},
-		},
-		Status: corev1.NodeStatus{
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: 0,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-		},
-	}
-	baseVNode = corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-		},
-		Status: corev1.NodeStatus{
-			Addresses: []corev1.NodeAddress{
-				{
-					Address: GetNodeHost(baseName.Name),
-					Type:    corev1.NodeHostName,
-				},
-			},
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: constants.KubeletPort,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-		},
-	}
-	editedNode := corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-			Annotations: map[string]string{
-				translate.ManagedLabelsAnnotation: "test",
-			},
-			Labels: map[string]string{
-				"test": "true",
-			},
-		},
-		Status: corev1.NodeStatus{
-			Addresses: []corev1.NodeAddress{
-				{
-					Address: GetNodeHost(baseName.Name),
-					Type:    corev1.NodeHostName,
-				},
-			},
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: constants.KubeletPort,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-		},
-	}
-
-	syncertesting.RunTests(t, []*syncertesting.SyncTest{
-		{
-			Name:                 "Label Not Matched and enforceNodeSelector false - expect node to be synced from pod needs",
-			InitialPhysicalState: []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			InitialVirtualState:  []runtime.Object{basePod.DeepCopy(), baseVNode.DeepCopy()},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{
-				corev1.SchemeGroupVersion.WithKind("Node"): {editedNode.DeepCopy()},
-				corev1.SchemeGroupVersion.WithKind("Pod"):  {basePod.DeepCopy()},
-			},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				ctx.Config.Networking.Advanced.ProxyKubelets.ByIP = false
-				ctx.Config.Sync.FromHost.Nodes.Selector.Labels = map[string]string{
-					"test": "true",
-				}
-				syncCtx, syncer := newFakeSyncer(t, ctx)
-				_, err := syncer.Sync(syncCtx, baseNode.DeepCopy(), baseVNode.DeepCopy())
-				assert.NilError(t, err)
-			},
-		},
-		{
-			Name:                 "Label Not Matched and enforceNodeSelector true - expect node not to be synced",
-			InitialPhysicalState: []runtime.Object{basePod.DeepCopy(), baseNode.DeepCopy()},
-			InitialVirtualState:  []runtime.Object{baseVNode.DeepCopy()},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				ctx.Config.Networking.Advanced.ProxyKubelets.ByIP = false
-				ctx.Config.Sync.FromHost.Nodes.Selector.Labels = map[string]string{
-					"test": "true",
-				}
-				syncCtx, syncer := newFakeSyncer(t, ctx)
-				_, err := syncer.Sync(syncCtx, baseNode.DeepCopy(), baseVNode.DeepCopy())
-				assert.NilError(t, err)
-			},
-		},
-	})
-}
-
-func TestClearNode(t *testing.T) {
-	baseNode = corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-			Labels: map[string]string{
-				"test": "true",
-			},
-		},
-		Status: corev1.NodeStatus{
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: 0,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-			Images: []corev1.ContainerImage{
-				{
-					Names: []string{"ghcr.io/jetpack/calico"},
-				},
-			},
-		},
-	}
-	baseVNode = corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-		},
-		Status: corev1.NodeStatus{
-			Addresses: []corev1.NodeAddress{
-				{
-					Address: GetNodeHost(baseName.Name),
-					Type:    corev1.NodeHostName,
-				},
-			},
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: constants.KubeletPort,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-		},
-	}
-	editedNode := corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-			Annotations: map[string]string{
-				translate.ManagedLabelsAnnotation: "test",
-			},
-			Labels: map[string]string{
-				"test": "true",
-			},
-		},
-		Status: corev1.NodeStatus{
-			Addresses: []corev1.NodeAddress{
-				{
-					Address: GetNodeHost(baseName.Name),
-					Type:    corev1.NodeHostName,
-				},
-			},
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: constants.KubeletPort,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-			Images: []corev1.ContainerImage{},
-		},
-	}
-
-	syncertesting.RunTests(t, []*syncertesting.SyncTest{
-		{
-			Name:                 "Clear Node Images Enabled -- Synced Node Should have no images in status.images",
-			InitialPhysicalState: []runtime.Object{baseNode.DeepCopy()},
-			InitialVirtualState:  []runtime.Object{baseVNode.DeepCopy()},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{
-				corev1.SchemeGroupVersion.WithKind("Node"): {editedNode.DeepCopy()},
-			},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				ctx.Config.Networking.Advanced.ProxyKubelets.ByIP = false
-				ctx.Config.Sync.FromHost.Nodes.Selector.All = true
-				ctx.Config.Sync.FromHost.Nodes.ClearImageStatus = true
-				syncCtx, syncerSvc := newFakeSyncer(t, ctx)
-				_, err := syncerSvc.Sync(syncCtx, baseNode.DeepCopy(), baseVNode.DeepCopy())
-				assert.NilError(t, err)
-			},
-		},
-	})
-}
-
-func TestClearNodeImageDisabled(t *testing.T) {
-	editedNode := &corev1.Node{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: baseName.Name,
-			Annotations: map[string]string{
-				translate.ManagedLabelsAnnotation: "test",
-			},
-			Labels: map[string]string{
-				"test": "true",
-			},
-		},
-		Status: corev1.NodeStatus{
-			Addresses: []corev1.NodeAddress{
-				{
-					Address: GetNodeHost(baseName.Name),
-					Type:    corev1.NodeHostName,
-				},
-			},
-			DaemonEndpoints: corev1.NodeDaemonEndpoints{
-				KubeletEndpoint: corev1.DaemonEndpoint{
-					Port: constants.KubeletPort,
-				},
-			},
-			NodeInfo: corev1.NodeSystemInfo{
-				Architecture: "amd64",
-			},
-			Images: []corev1.ContainerImage{
-				{
-					Names: []string{"ghcr.io/jetpack/calico"},
-				},
-			},
-		},
-	}
-
-	syncertesting.RunTests(t, []*syncertesting.SyncTest{
-		{
-			Name:                 "Clear Node Images Disabled -- Synced Node Should have images in status.images",
-			InitialPhysicalState: []runtime.Object{baseNode.DeepCopy()},
-			InitialVirtualState:  []runtime.Object{baseVNode.DeepCopy()},
-			ExpectedVirtualState: map[schema.GroupVersionKind][]runtime.Object{
-				corev1.SchemeGroupVersion.WithKind("Node"): {editedNode.DeepCopy()},
-			},
-			Sync: func(ctx *synccontext.RegisterContext) {
-				ctx.Config.Networking.Advanced.ProxyKubelets.ByIP = false
-				ctx.Config.Sync.FromHost.Nodes.Selector.All = true
-				syncCtx, syncerSvc := newFakeSyncer(t, ctx)
-				_, err := syncerSvc.Sync(syncCtx, baseNode.DeepCopy(), baseVNode.DeepCopy())
-				assert.NilError(t, err)
-			},
-		},
-	})
 }
