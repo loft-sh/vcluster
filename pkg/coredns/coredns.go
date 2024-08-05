9b23c74d8a5251613a01947e52780b5fe96b2d0a
Paweł Bojanowski <pbojanowski@protonmail.com>
2024-08-05 08:58:40 +0200

    bump default coredns image to v1.11.3 (because previous ones had CVEs)

--- pkg/coredns/coredns.go
+++ pkg/coredns/coredns.go
@@ -16,7 +16,7 @@ import (
 )
 
 const (
-	DefaultImage          = "coredns/coredns:1.11.1"
+	DefaultImage          = "coredns/coredns:1.11.3"
 	ManifestRelativePath  = "coredns/coredns.yaml"
 	ManifestsOutputFolder = "/tmp/manifests-to-apply"
 	VarImage              = "IMAGE"
