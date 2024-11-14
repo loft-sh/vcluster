8629961d000e5de0410059fc9be8ebf4850337e1
Rohan CJ <rohantmp@gmail.com>
2024-11-14 16:12:48 +0530

    chore: remove ununused LabelSelectorRequirement
    Signed-off-by: Rohan CJ <rohantmp@gmail.com>

--- config/config.go
+++ config/config.go
@@ -1616,21 +1616,6 @@ type ResourceQuota struct {
 	LabelsAndAnnotations `json:",inline"`
 }
 
-type LabelSelectorRequirement struct {
-	// key is the label key that the selector applies to.
-	Key string `json:"key"`
-
-	// operator represents a key's relationship to a set of values.
-	// Valid operators are In, NotIn, Exists and DoesNotExist.
-	Operator string `json:"operator"`
-
-	// values is an array of string values. If the operator is In or NotIn,
-	// the values array must be non-empty. If the operator is Exists or DoesNotExist,
-	// the values array must be empty. This array is replaced during a strategic
-	// merge patch.
-	Values []string `json:"values,omitempty"`
-}
-
 type LimitRange struct {
 	// Enabled defines if the limit range should be deployed by vCluster. "auto" means that if resourceQuota is enabled,
 	// the limitRange will be enabled as well.
