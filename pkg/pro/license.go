0eb47ff7338c560f2648093395faeae300c15a1d
Paweł Bojanowski <pawel.bojanowski@loft.sh>
2024-12-06 14:26:22 +0100

    adjust license pro functions to the changes made in the vcluster-pro refactor
    Signed-off-by: Paweł Bojanowski <pawel.bojanowski@loft.sh>

--- pkg/pro/license.go
+++ pkg/pro/license.go
@@ -4,14 +4,16 @@ import (
 	"context"
 
 	"github.com/loft-sh/vcluster/pkg/config"
+	"github.com/loft-sh/vcluster/pkg/syncer/synccontext"
 )
 
-// LicenseInit is used to initialize the license reader
-var LicenseInit = func(_ context.Context, _ *config.VirtualClusterConfig) error {
+// LicenseInit is used to initialize the license loader
+var LicenseInit = func(_ context.Context, _ *config.VirtualClusterConfig) {}
+
+// LicenseStart is used to start license loader
+var LicenseStart = func(_ *synccontext.ControllerContext) error {
 	return nil
 }
 
-// LicenseFeatures is used to retrieve all enabled features
-var LicenseFeatures = func() map[string]bool {
-	return make(map[string]bool)
-}
+// LicenseFeatures returns a map of featureName: enabled / disabled
+var LicenseFeatures func() map[string]bool
