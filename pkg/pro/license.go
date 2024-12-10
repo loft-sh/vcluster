1484731a99c02fca8984b77250d3d3fa07466190
Paweł Bojanowski <pawel.bojanowski@loft.sh>
2024-12-10 16:48:35 +0100

    vcluster license initalization / loader refactor (#2320)
    * adjust license pro functions to the changes made in the vcluster-pro refactor
    
    Signed-off-by: Paweł Bojanowski <pawel.bojanowski@loft.sh>
    
    * return error from license.Init
    
    Signed-off-by: Paweł Bojanowski <pawel.bojanowski@loft.sh>
    
    * refactor: use delegate for platform authentication
    
    ---------
    
    Signed-off-by: Paweł Bojanowski <pawel.bojanowski@loft.sh>
    Co-authored-by: Fabian Kramm <fab.kramm@googlemail.com>

--- pkg/pro/license.go
+++ pkg/pro/license.go
@@ -4,14 +4,20 @@ import (
 	"context"
 
 	"github.com/loft-sh/vcluster/pkg/config"
+	"github.com/loft-sh/vcluster/pkg/syncer/synccontext"
 )
 
-// LicenseInit is used to initialize the license reader
+// LicenseInit is used to initialize the license loader
 var LicenseInit = func(_ context.Context, _ *config.VirtualClusterConfig) error {
 	return nil
 }
 
-// LicenseFeatures is used to retrieve all enabled features
+// LicenseStart is used to start license loader
+var LicenseStart = func(_ *synccontext.ControllerContext) error {
+	return nil
+}
+
+// LicenseFeatures returns a map of featureName: enabled / disabled
 var LicenseFeatures = func() map[string]bool {
 	return make(map[string]bool)
 }
