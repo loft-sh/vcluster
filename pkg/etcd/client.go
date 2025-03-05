ffe47e295a3987bfc0f8e8237dee4a7a2a585c7d
Matej Ohradzansky <matohradzansky@gmail.com>
2025-03-05 09:56:13 +0000

    support external etcd in all distros
    Signed-off-by: Matej Ohradzansky <matohradzansky@gmail.com>

--- pkg/etcd/client.go
+++ pkg/etcd/client.go
@@ -65,19 +65,17 @@ func GetEtcdEndpoint(vConfig *config.VirtualClusterConfig) (string, *Certificate
 		} else {
 			etcdEndpoints = "https://" + vConfig.Name + "-etcd-headless:2379"
 		}
+	} else if vConfig.ControlPlane.BackingStore.Etcd.External.Enabled {
+		etcdEndpoints = vConfig.ControlPlane.BackingStore.Etcd.External.Service
+		etcdCertificates = &Certificates{
+			CaCert:     vConfig.ControlPlane.BackingStore.Etcd.External.Certificate.CaFile,
+			ServerCert: vConfig.ControlPlane.BackingStore.Etcd.External.Certificate.CrtFile,
+			ServerKey:  vConfig.ControlPlane.BackingStore.Etcd.External.Certificate.KeyFile,
+		}
 	} else if vConfig.Distro() == vconfig.K8SDistro {
 		etcdEndpoints = constants.K8sKineEndpoint
 	} else if vConfig.Distro() == vconfig.K3SDistro {
-		if vConfig.ControlPlane.BackingStore.Etcd.External.Enabled {
-			etcdEndpoints = vConfig.ControlPlane.BackingStore.Etcd.External.Service
-			etcdCertificates = &Certificates{
-				CaCert:     vConfig.ControlPlane.BackingStore.Etcd.External.Certificate.CaFile,
-				ServerCert: vConfig.ControlPlane.BackingStore.Etcd.External.Certificate.CrtFile,
-				ServerKey:  vConfig.ControlPlane.BackingStore.Etcd.External.Certificate.KeyFile,
-			}
-		} else {
-			etcdEndpoints = constants.K3sKineEndpoint
-		}
+		etcdEndpoints = constants.K3sKineEndpoint
 	} else if vConfig.Distro() == vconfig.K0SDistro {
 		if (vConfig.ControlPlane.BackingStore.Database.Embedded.Enabled && vConfig.ControlPlane.BackingStore.Database.Embedded.DataSource != "") ||
 			vConfig.ControlPlane.BackingStore.Database.External.Enabled {
