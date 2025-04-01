af28696d1fecdf9088b2904c08bc12b019a8f8a1
Nikola Prokopić <nikola.prokopic@loft.sh>
2025-04-01 16:57:47 +0200

    Test Reconcile when host service is synced to new virtual service

--- pkg/controllers/servicesync/servicesync_test.go
+++ pkg/controllers/servicesync/servicesync_test.go
@@ -7,7 +7,11 @@ import (
 	syncertesting "github.com/loft-sh/vcluster/pkg/syncer/testing"
 	"github.com/loft-sh/vcluster/pkg/util/loghelper"
 	testingutil "github.com/loft-sh/vcluster/pkg/util/testing"
+	"github.com/loft-sh/vcluster/pkg/util/translate"
 	"gotest.tools/v3/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
@@ -44,9 +48,11 @@ func TestFromHostRegister(t *testing.T) {
 
 func TestFromHostReconcile(t *testing.T) {
 	testCases := []struct {
-		Name     string
-		Mappings map[string]types.NamespacedName
-		Request  ctrl.Request
+		Name                    string
+		Mappings                map[string]types.NamespacedName
+		Request                 ctrl.Request
+		InitialHostServices     []runtime.Object
+		ExpectedVirtualServices []runtime.Object
 	}{
 		{
 			Name: "Service is not synced",
@@ -78,15 +84,70 @@ func TestFromHostReconcile(t *testing.T) {
 				},
 			},
 		},
+		{
+			Name: "Sync host service to new virtual service",
+			InitialHostServices: []runtime.Object{
+				&corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "host-namespace",
+						Name:      "host-service",
+					},
+					Spec: corev1.ServiceSpec{
+						Selector: map[string]string{
+							"app": "host-app",
+						},
+						Ports: []corev1.ServicePort{
+							{
+								Name: "http",
+								Port: 8080,
+							},
+						},
+					},
+				},
+			},
+			ExpectedVirtualServices: []runtime.Object{
+				&corev1.Service{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "virtual-namespace",
+						Name:      "virtual-service",
+						Labels: map[string]string{
+							translate.ControllerLabel: "vcluster",
+						},
+					},
+					Spec: corev1.ServiceSpec{
+						ClusterIP: "None",
+						Ports: []corev1.ServicePort{
+							{
+								Name: "http",
+								Port: 8080,
+							},
+						},
+					},
+				},
+			},
+			Mappings: map[string]types.NamespacedName{
+				"host-namespace/host-service": {
+					Namespace: "virtual-namespace",
+					Name:      "virtual-service",
+				},
+			},
+			Request: ctrl.Request{
+				NamespacedName: types.NamespacedName{
+					Namespace: "host-namespace",
+					Name:      "host-service",
+				},
+			},
+		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.Name, func(t *testing.T) {
 			name := "test-map-host-service-syncer"
-			pClient := testingutil.NewFakeClient(scheme.Scheme)
+			pClient := testingutil.NewFakeClient(scheme.Scheme, testCase.InitialHostServices...)
 			vClient := testingutil.NewFakeClient(scheme.Scheme)
 			fakeConfig := testingutil.NewFakeConfig()
 			fakeContext := syncertesting.NewFakeRegisterContext(fakeConfig, pClient, vClient)
+			serviceGVK := corev1.SchemeGroupVersion.WithKind("Service")
 
 			// create new FromHost syncer
 			serviceSyncer := &ServiceSyncer{
@@ -106,6 +167,22 @@ func TestFromHostReconcile(t *testing.T) {
 
 			// Check that reconcile executes without errors
 			assert.NilError(t, err)
+
+			// Check expected resources
+			if testCase.ExpectedVirtualServices != nil {
+				compareErr := syncertesting.CompareObjs(
+					fakeContext,
+					t,
+					testCase.Name+" virtual state",
+					fakeContext.VirtualManager.GetClient(),
+					serviceGVK,
+					scheme.Scheme,
+					testCase.ExpectedVirtualServices,
+					nil)
+				if err != nil {
+					t.Fatalf("%s - Virtual State mismatch %v", testCase.Name, compareErr)
+				}
+			}
 		})
 	}
 }
