af3378f08227d7d2584c9b630db5eeabea4abd79
Nikola Prokopić <nikola.prokopic@loft.sh>
2025-04-01 14:57:00 +0200

    Add basic test for ServiceSyncer.Register

---
+++ pkg/controllers/servicesync/servicesync_test.go
@@ -0,0 +1,39 @@
+package servicesync
+
+import (
+	"testing"
+
+	"github.com/loft-sh/vcluster/pkg/scheme"
+	syncertesting "github.com/loft-sh/vcluster/pkg/syncer/testing"
+	"github.com/loft-sh/vcluster/pkg/util/loghelper"
+	testingutil "github.com/loft-sh/vcluster/pkg/util/testing"
+	"gotest.tools/v3/assert"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestRegister(t *testing.T) {
+	name := "test-map-host-service-syncer"
+	pClient := testingutil.NewFakeClient(scheme.Scheme)
+	vClient := testingutil.NewFakeClient(scheme.Scheme)
+	fakeConfig := testingutil.NewFakeConfig()
+	fakeContext := syncertesting.NewFakeRegisterContext(fakeConfig, pClient, vClient)
+	fakeMapping := map[string]types.NamespacedName{
+		"host-namespace/host-service": {
+			Namespace: "virtual-namespace",
+			Name:      "virtual-service",
+		},
+	}
+	serviceSyncer := &ServiceSyncer{
+		Name:            name,
+		SyncContext:     fakeContext.ToSyncContext(name),
+		SyncServices:    fakeMapping,
+		CreateNamespace: true,
+		CreateEndpoints: true,
+		From:            fakeContext.PhysicalManager,
+		To:              fakeContext.VirtualManager,
+		Log:             loghelper.New(name),
+	}
+
+	err := serviceSyncer.Register()
+	assert.NilError(t, err)
+}
