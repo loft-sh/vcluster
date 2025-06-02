9282ce2fe86826d414306bb1f97a38c5a7f2df31
Fabian Kramm <fab.kramm@googlemail.com>
2025-06-02 13:19:29 +0200

    refactor: move coredns configmap into code (#2864)

--- pkg/coredns/coredns.go
+++ pkg/coredns/coredns.go
@@ -1,14 +1,12 @@
 package coredns
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"os"
-	"path"
 	"strings"
-	"text/template"
 
+	"github.com/loft-sh/vcluster/config"
 	"github.com/loft-sh/vcluster/pkg/constants"
 	"github.com/loft-sh/vcluster/pkg/util/applier"
 	"k8s.io/apimachinery/pkg/version"
@@ -16,50 +14,34 @@ import (
 )
 
 const (
-	DefaultImage          = "coredns/coredns:1.11.3"
-	ManifestRelativePath  = "coredns/coredns.yaml"
-	ManifestsOutputFolder = "/tmp/manifests-to-apply"
-	VarImage              = "IMAGE"
-	VarHostDNS            = "HOST_CLUSTER_DNS"
-	VarRunAsUser          = "RUN_AS_USER"
-	VarRunAsNonRoot       = "RUN_AS_NON_ROOT"
-	VarRunAsGroup         = "RUN_AS_GROUP"
-	VarLogInDebug         = "LOG_IN_DEBUG"
-	defaultUID            = int64(1001)
-	defaultGID            = int64(1001)
+	DefaultImage    = "coredns/coredns:1.11.3"
+	VarImage        = "IMAGE"
+	VarHostDNS      = "HOST_CLUSTER_DNS"
+	VarRunAsUser    = "RUN_AS_USER"
+	VarRunAsNonRoot = "RUN_AS_NON_ROOT"
+	VarRunAsGroup   = "RUN_AS_GROUP"
+	VarLogInDebug   = "LOG_IN_DEBUG"
+	defaultUID      = int64(1001)
+	defaultGID      = int64(1001)
 )
 
 var ErrNoCoreDNSManifests = fmt.Errorf("no coredns manifests found")
 
-func ApplyManifest(ctx context.Context, defaultImageRegistry string, inClusterConfig *rest.Config, serverVersion *version.Info) error {
+func ApplyManifest(ctx context.Context, config *config.Config, defaultImageRegistry string, inClusterConfig *rest.Config, serverVersion *version.Info) error {
+	if !config.ControlPlane.CoreDNS.Enabled {
+		return nil
+	}
+
+	// get the manifest variables
 	vars := getManifestVariables(defaultImageRegistry, serverVersion)
-	output, err := processManifestTemplate(vars)
+
+	// process the corefile and manifests
+	output, err := processManifests(vars, config)
 	if err != nil {
 		return err
 	}
 
-	// write manifest into a file for easier debugging
-	if os.Getenv("DEBUG") == "true" {
-		// create a temporary directory and file to output processed manifest to
-		debugOutputFile, err := prepareManifestOutput()
-		if err != nil {
-			return err
-		}
-		defer debugOutputFile.Close()
-
-		_, _ = debugOutputFile.Write(output)
-	}
-
 	return applier.ApplyManifest(ctx, inClusterConfig, output)
-}
-
-func prepareManifestOutput() (*os.File, error) {
-	manifestOutputPath := path.Join(ManifestsOutputFolder, ManifestRelativePath)
-	err := os.MkdirAll(path.Dir(manifestOutputPath), 0755)
-	if err != nil {
-		return nil, err
-	}
-	return os.Create(manifestOutputPath)
 }
 
 func getManifestVariables(defaultImageRegistry string, serverVersion *version.Info) map[string]interface{} {
@@ -117,22 +99,4 @@ func GetUserID() int64 {
 	}
 
 	return int64(uid)
-}
-
-func processManifestTemplate(vars map[string]interface{}) ([]byte, error) {
-	manifestInputPath := path.Join("/manifests", ManifestRelativePath)
-	// check if the manifestInputPath exists
-	if _, err := os.Stat(manifestInputPath); os.IsNotExist(err) {
-		return nil, ErrNoCoreDNSManifests
-	}
-	manifestTemplate, err := template.ParseFiles(manifestInputPath)
-	if err != nil {
-		return nil, fmt.Errorf("unable to parse %s: %w", manifestInputPath, err)
-	}
-	buf := new(bytes.Buffer)
-	err = manifestTemplate.Execute(buf, vars)
-	if err != nil {
-		return nil, fmt.Errorf("manifestTemplate.Execute failed for manifest %s: %w", manifestInputPath, err)
-	}
-	return buf.Bytes(), nil
 }
