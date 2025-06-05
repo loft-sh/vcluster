45446a509153ce3171a64906efd046eaa0a87ac7
Fabian Kramm <fab.kramm@googlemail.com>
2025-06-05 11:46:24 +0200

    fix: allow setting vcluster name for standalone mode

--- cmd/vcluster/cmd/start.go
+++ cmd/vcluster/cmd/start.go
@@ -50,7 +50,7 @@ func NewStartCommand() *cobra.Command {
 }
 
 func ExecuteStart(ctx context.Context, options *StartOptions) error {
-	if os.Getenv("VCLUSTER_NAME") == "" && os.Getenv("POD_NAME") == "" && os.Getenv("POD_NAMESPACE") == "" {
+	if os.Getenv("POD_NAME") == "" && os.Getenv("POD_NAMESPACE") == "" {
 		return pro.StartStandalone(ctx, &pro.StandaloneOptions{
 			Config: options.Config,
 		})
