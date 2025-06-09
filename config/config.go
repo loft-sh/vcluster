322e5b56c9be20d81817f541fd2a5083f0f3a185
Amruta Kale <amruta.kale@loft.sh>
2025-06-09 13:43:45 +0530

    separate out structs for all the probes

--- config/config.go
+++ config/config.go
@@ -1977,21 +1977,44 @@ type ControlPlaneWorkloadServiceAccount struct {
 
 type ControlPlaneProbes struct {
 	// LivenessProbe specifies if the liveness probe for the container should be enabled
-	LivenessProbe Probe `json:"livenessProbe,omitempty"`
+	LivenessProbe LivenessProbe `json:"livenessProbe,omitempty"`
 
 	// ReadinessProbe specifies if the readiness probe for the container should be enabled
-	ReadinessProbe Probe `json:"readinessProbe,omitempty"`
+	ReadinessProbe ReadinessProbe `json:"readinessProbe,omitempty"`
 
 	// StartupProbe specifies if the startup probe for the container should be enabled
-	StartupProbe Probe `json:"startupProbe,omitempty"`
+	StartupProbe StartupProbe `json:"startupProbe,omitempty"`
 }
 
-type Probe struct {
+// LivenessProbe defines the configuration for the liveness probe.
+// A liveness probe checks if the container is still running.
+// If it fails, Kubernetes will restart the container.
+type LivenessProbe struct {
 	EnableSwitch
-	FailureThreshold    int `json:"failureThreshold,omitempty"`
-	InitialDelaySeconds int `json:"initialDelaySeconds,omitempty"`
-	TimeoutSeconds      int `json:"timeoutSeconds,omitempty"`
-	PeriodSeconds       int `json:"periodSeconds,omitempty"`
+	FailureThreshold    int `json:"failureThreshold,omitempty"`    // Number of consecutive failures for the probe to be considered failed
+	InitialDelaySeconds int `json:"initialDelaySeconds,omitempty"` // Time (in seconds) to wait before starting the liveness probe
+	TimeoutSeconds      int `json:"timeoutSeconds,omitempty"`      // Maximum duration (in seconds) that the probe will wait for a response.
+	PeriodSeconds       int `json:"periodSeconds,omitempty"`       // Frequency (in seconds) to perform the probe
+}
+
+// ReadinessProbe defines the configuration for the readiness probe.
+// A readiness probe checks if the container is ready to accept traffic.
+// If it fails, Kubernetes removes the pod from the Service endpoints.
+type ReadinessProbe struct {
+	EnableSwitch
+	FailureThreshold int `json:"failureThreshold,omitempty"` // Number of consecutive failures for the probe to be considered failed
+	TimeoutSeconds   int `json:"timeoutSeconds,omitempty"`   // Maximum duration (in seconds) that the probe will wait for a response.
+	PeriodSeconds    int `json:"periodSeconds,omitempty"`    // Frequency (in seconds) to perform the probe
+}
+
+// StartupProbe defines the configuration for the startup probe.
+// A startup probe checks if the application within the container has started.
+// While the startup probe is failing, other probes are disabled.
+type StartupProbe struct {
+	EnableSwitch
+	FailureThreshold int `json:"failureThreshold,omitempty"` // Number of consecutive failures allowed before failing the pod
+	TimeoutSeconds   int `json:"timeoutSeconds,omitempty"`   // Maximum duration (in seconds) that the probe will wait for a response.
+	PeriodSeconds    int `json:"periodSeconds,omitempty"`    // Frequency (in seconds) to perform the probe
 }
 
 type ControlPlaneSecurity struct {
