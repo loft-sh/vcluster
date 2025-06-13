79e3410e7fa0f358c5b73ce182bea052f404a353
Jan Baraniewski <dev@baraniewski.com>
2025-06-13 15:56:48 +0200

    Fix lint issues

--- pkg/cli/cleanup_namespaces.go
+++ pkg/cli/cleanup_namespaces.go
@@ -26,9 +26,9 @@ const (
 	vClusterMetadataPrefix = "vcluster.loft.sh/"
 )
 
-// CleanupSyncedNamespaces identifies all physical namespaces that were created or managed by vCluster.
-//  1. Namespaces created by the vCluster: These are deleted directly.
-//  2. Namespaces "imported" into the vCluster: For these, we remove all vCluster-related
+// CleanupSyncedNamespaces identifies all physical namespaces that were managed by this vCluster.
+//  1. Namespaces created on host as result of vCluster syncing will be deleted.
+//  2. Namespaces imported into the vCluster from host will be cleaned up by removing all vCluster-related
 //     metadata from the namespace itself and all resources within it.
 func CleanupSyncedNamespaces(
 	ctx context.Context,
@@ -40,7 +40,8 @@ func CleanupSyncedNamespaces(
 ) error {
 	logger.Infof("Starting cleanup of vCluster '%s' namespaces.", vClusterName)
 
-	managedNamespaces, err := getManagedNamespaces(ctx, k8sClient, mainPhysicalNamespace, vClusterName, logger)
+	// Fetch namespaces managed by this vCluster.
+	managedNamespaces, err := getManagedNamespaces(ctx, k8sClient, mainPhysicalNamespace, vClusterName)
 	if err != nil {
 		return err
 	}
@@ -49,11 +50,13 @@ func CleanupSyncedNamespaces(
 		return nil
 	}
 
+	// Build dynamic client for cleaning up imported resources.
 	dynClient, err := dynamic.NewForConfig(restConfig)
 	if err != nil {
 		return err
 	}
 
+	// Fetch all API namespaced resources.
 	namespacedResources, err := discoverNamespacedResources(k8sClient.Discovery(), logger)
 	if err != nil {
 		return fmt.Errorf("failed to discover API resources: %w", err)
@@ -66,7 +69,7 @@ func CleanupSyncedNamespaces(
 				errs = append(errs, err)
 			}
 		} else {
-			err := deleteAndLogNamespace(ctx, k8sClient, ns.Name, logger)
+			err := deleteNamespace(ctx, k8sClient, ns.Name, logger)
 			if err != nil {
 				errs = append(errs, err)
 			}
@@ -81,6 +84,7 @@ func CleanupSyncedNamespaces(
 	return nil
 }
 
+// cleanupImportedNamespace cleans up metadata of namespace resource and all resources running in this namespace.
 func cleanupImportedNamespace(ctx context.Context, dynClient dynamic.Interface, ns *corev1.Namespace, resources []schema.GroupVersionResource, logger log.Logger) error {
 	logger.Infof("Namespace '%s' was imported, cleaning up its resources and metadata...", ns.Name)
 
@@ -93,13 +97,13 @@ func cleanupImportedNamespace(ctx context.Context, dynClient dynamic.Interface, 
 	if err != nil {
 		return err
 	}
-	if err := cleanupObjectMetadata(ctx, nsInterface, &unstructured.Unstructured{Object: unstructuredNS}, logger); err != nil {
+	if err := cleanupObjectMetadata(ctx, nsInterface, &unstructured.Unstructured{Object: unstructuredNS}); err != nil {
 		errs = append(errs, err)
 	}
 
 	// Then, iterate over all discovered namespaced resource types and clean them up.
 	for _, gvr := range resources {
-		err := cleanupObjectsByGVR(ctx, dynClient, ns.Name, gvr, logger)
+		err := cleanupObjectsByGVR(ctx, dynClient, ns.Name, gvr)
 		if err != nil {
 			errs = append(errs, err)
 		}
@@ -111,7 +115,8 @@ func cleanupImportedNamespace(ctx context.Context, dynClient dynamic.Interface, 
 	return nil
 }
 
-func getManagedNamespaces(ctx context.Context, k8sClient *kubernetes.Clientset, mainPhysicalNamespace, vClusterName string, logger log.Logger) ([]corev1.Namespace, error) {
+// getManagedNamespaces fetches all namespaces managed by this vCluster.
+func getManagedNamespaces(ctx context.Context, k8sClient *kubernetes.Clientset, mainPhysicalNamespace, vClusterName string) ([]corev1.Namespace, error) {
 	labelSelector := translate.MarkerLabel + "=" + translate.SafeConcatName(mainPhysicalNamespace, "x", vClusterName)
 	nsList, err := k8sClient.CoreV1().Namespaces().List(ctx, metav1.ListOptions{LabelSelector: labelSelector})
 	if err != nil {
@@ -120,7 +125,8 @@ func getManagedNamespaces(ctx context.Context, k8sClient *kubernetes.Clientset, 
 	return nsList.Items, nil
 }
 
-func deleteAndLogNamespace(ctx context.Context, k8sClient *kubernetes.Clientset, nsName string, logger log.Logger) error {
+// deleteNamespace deletes namespace from host.
+func deleteNamespace(ctx context.Context, k8sClient *kubernetes.Clientset, nsName string, logger log.Logger) error {
 	logger.Infof("Deleting virtual cluster namespace '%s'", nsName)
 	err := k8sClient.CoreV1().Namespaces().Delete(ctx, nsName, metav1.DeleteOptions{})
 	if err != nil {
@@ -139,6 +145,7 @@ func isImportedNamespace(ns *corev1.Namespace) bool {
 	return ns.Annotations != nil && ns.Annotations[translate.ImportedMarkerAnnotation] == "true"
 }
 
+// discoverNamespacedResources retrieves list of namespaced GVRs from API.
 func discoverNamespacedResources(discoveryClient discovery.DiscoveryInterface, logger log.Logger) ([]schema.GroupVersionResource, error) {
 	resourceList, err := discoveryClient.ServerPreferredResources()
 	if err != nil {
@@ -184,9 +191,10 @@ func discoverNamespacedResources(discoveryClient discovery.DiscoveryInterface, l
 	return patchableResources, nil
 }
 
-func cleanupObjectsByGVR(ctx context.Context, dynClient dynamic.Interface, namespace string, gvr schema.GroupVersionResource, logger log.Logger) error {
-	resourceInterface := dynClient.Resource(gvr).Namespace(namespace)
-	objects, err := resourceInterface.List(ctx, metav1.ListOptions{})
+// cleanupObjectsByGVR lists all objects of given GVR and runs metadata cleanup on them.
+func cleanupObjectsByGVR(ctx context.Context, dynClient dynamic.Interface, namespace string, gvr schema.GroupVersionResource) error {
+	resourceClient := dynClient.Resource(gvr).Namespace(namespace)
+	objects, err := resourceClient.List(ctx, metav1.ListOptions{})
 	if err != nil {
 		if kerrors.IsNotFound(err) || kerrors.IsForbidden(err) {
 			return nil
@@ -200,7 +208,7 @@ func cleanupObjectsByGVR(ctx context.Context, dynClient dynamic.Interface, names
 
 	var errs []error
 	for i := range objects.Items {
-		if err := cleanupObjectMetadata(ctx, resourceInterface, &objects.Items[i], logger); err != nil {
+		if err := cleanupObjectMetadata(ctx, resourceClient, &objects.Items[i]); err != nil {
 			errs = append(errs, err)
 		}
 	}
@@ -211,13 +219,15 @@ func cleanupObjectsByGVR(ctx context.Context, dynClient dynamic.Interface, names
 	return nil
 }
 
-func cleanupObjectMetadata(ctx context.Context, resInterface dynamic.ResourceInterface, obj *unstructured.Unstructured, logger log.Logger) error {
+// cleanupObjectMetadata checks given object for vCluster labels and annotations and runs patch operation removing them.
+func cleanupObjectMetadata(ctx context.Context, client dynamic.ResourceInterface, obj *unstructured.Unstructured) error {
 	type jsonPatch struct {
 		Op   string `json:"op"`
 		Path string `json:"path"`
 	}
 	var patch []jsonPatch
 
+	// labels/annotations need to be escaped for patches
 	jsonpointer := strings.NewReplacer("~", "~0", "/", "~1")
 
 	// Check all labels for the prefix and add a "remove" operation to the patch if found.
@@ -241,13 +251,14 @@ func cleanupObjectMetadata(ctx context.Context, resInterface dynamic.ResourceInt
 		return nil
 	}
 
+	// Create patch.
 	patchBytes, err := json.Marshal(patch)
 	if err != nil {
 		return err
 	}
 
 	// Apply the patch to the object.
-	_, err = resInterface.Patch(ctx, obj.GetName(), types.JSONPatchType, patchBytes, metav1.PatchOptions{})
+	_, err = client.Patch(ctx, obj.GetName(), types.JSONPatchType, patchBytes, metav1.PatchOptions{})
 	if err != nil {
 		if kerrors.IsNotFound(err) {
 			// Object might have been deleted by another process in the meantime, which is fine.
