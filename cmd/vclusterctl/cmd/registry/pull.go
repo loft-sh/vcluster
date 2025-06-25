0b9033b4567e0af43a4db2b095b2a3895b9cdcdc
Fabian Kramm <fab.kramm@googlemail.com>
2025-06-25 18:23:11 +0200

    refactor: use skopeo for registry cmds (#2957)

--- cmd/vclusterctl/cmd/registry/pull.go
+++ cmd/vclusterctl/cmd/registry/pull.go
@@ -3,27 +3,25 @@ package registry
 import (
 	"context"
 	"fmt"
-	"net/http"
-	"net/url"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 
-	"github.com/docker/docker/pkg/jsonmessage"
-	"github.com/google/go-containerregistry/pkg/name"
-	v1 "github.com/google/go-containerregistry/pkg/v1"
-	"github.com/google/go-containerregistry/pkg/v1/remote"
-	"github.com/google/go-containerregistry/pkg/v1/tarball"
+	"github.com/loft-sh/image/copy"
+	"github.com/loft-sh/image/transports/alltransports"
+	"github.com/loft-sh/image/types"
 	"github.com/loft-sh/log"
 	"github.com/loft-sh/vcluster/pkg/cli/flags"
 	"github.com/spf13/cobra"
-	"k8s.io/client-go/rest"
 )
 
 type PullOptions struct {
 	*flags.GlobalFlags
 
 	Destination string
+
+	Architecture string
 
 	Log log.Logger
 }
@@ -37,7 +35,7 @@ func NewPullCmd(globalFlags *flags.GlobalFlags) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:   "pull",
-		Short: "Pull a local docker image or archive from vCluster registry",
+		Short: "Pull a docker image from a container registry and save it to a tarball that can be imported into the vCluster registry",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return o.Run(cmd.Context(), args)
@@ -45,119 +43,75 @@ func NewPullCmd(globalFlags *flags.GlobalFlags) *cobra.Command {
 	}
 
 	cmd.Flags().StringVar(&o.Destination, "destination", "", "Path to the destination directory. If not specified, the images will be pulled to the current directory.")
+	cmd.Flags().StringVar(&o.Architecture, "architecture", runtime.GOARCH, "Architecture of the image to pull. If not specified, the architecture will be detected automatically. Use 'all' to pull all architectures.")
 
 	return cmd
 }
 
 func (o *PullOptions) Run(ctx context.Context, images []string) error {
-	// get the client config
-	restConfig, err := getClient(o.GlobalFlags)
-	if err != nil {
-		return fmt.Errorf("failed to get client config: %w", err)
-	}
-
-	// get the transport
-	transport, err := rest.TransportFor(restConfig)
-	if err != nil {
-		return fmt.Errorf("failed to get transport: %w", err)
-	}
-
-	// check if registry is enabled
-	registryEnabled, err := isRegistryEnabled(ctx, restConfig.Host, transport)
-	if err != nil {
-		return fmt.Errorf("failed to check if registry is enabled: %w", err)
-	} else if !registryEnabled {
-		return fmt.Errorf("vCluster registry is not enabled or the target cluster is not a vCluster. Please make sure to enable the registry in the vCluster config and run `vcluster connect` to connect to the vCluster before pulling images")
-	}
-
-	// get the vCluster host
-	vClusterHost, err := url.Parse(restConfig.Host)
-	if err != nil {
-		return fmt.Errorf("failed to parse vCluster host: %w", err)
-	}
-
-	// get the remote registry
-	remoteRegistry, err := name.NewRegistry(vClusterHost.Host)
-	if err != nil {
-		return fmt.Errorf("failed to get remote registry: %w", err)
-	}
-
-	// pull the images
 	for _, image := range images {
-		// parse the image reference
-		localRef, err := name.ParseReference(image)
-		if err != nil {
-			return fmt.Errorf("failed to parse image reference: %w", err)
-		}
-
-		// pull the image
-		if err := o.pullImage(ctx, localRef, remoteRegistry, transport); err != nil {
-			return fmt.Errorf("failed to pull image %s: %w", localRef.String(), err)
+		if err := o.pullImage(ctx, image); err != nil {
+			return fmt.Errorf("failed to pull image %s: %w", image, err)
 		}
 	}
 
 	return nil
 }
 
-func (o *PullOptions) pullImage(ctx context.Context, localRef name.Reference, remoteRegistry name.Registry, transport http.RoundTripper) error {
-	remoteRef, err := replaceRegistry(localRef, remoteRegistry)
-	if err != nil {
-		return err
+func (o *PullOptions) pullImage(ctx context.Context, image string) error {
+	srcContext := &types.SystemContext{
+		OSChoice:                    "linux",
+		DockerInsecureSkipTLSVerify: types.OptionalBoolTrue,
+	}
+	destContext := &types.SystemContext{
+		OSChoice:                    "linux",
+		DockerInsecureSkipTLSVerify: types.OptionalBoolTrue,
 	}
 
-	// create the progress channel
-	progressChan := make(chan v1.Update, 200)
-	defer close(progressChan)
-
-	// pull the image
-	tarballPath := filepath.Join(o.Destination, refToFilename(localRef))
-	o.Log.Infof("Pulling image %s from %s to %s", localRef.Name(), remoteRef.Name(), tarballPath)
-	image, err := remote.Image(remoteRef, remote.WithContext(ctx), remote.WithProgress(progressChan), remote.WithTransport(transport))
+	// get the source reference
+	srcRef, err := alltransports.ParseImageName("docker://" + image)
 	if err != nil {
-		return fmt.Errorf("failed to get image: %w", err)
+		return fmt.Errorf("invalid source name %s: %w", image, err)
 	}
 
-	// create the tarball file
-	tarballFile, err := os.Create(tarballPath)
-	if err != nil {
-		return fmt.Errorf("failed to create tarball file: %w", err)
-	}
-	defer tarballFile.Close()
-
-	// write the progress to the console
-	go func() {
-		for update := range progressChan {
-			if update.Error != nil {
-				o.Log.Errorf("failed to pull image %s: %v", localRef.String(), update.Error)
-				return
-			}
-
-			status := "Pushing"
-			if update.Complete == update.Total {
-				status = "Pushed"
-			}
-
-			_, err := fmt.Fprintf(os.Stdout, "%s %s\n", status, (&jsonmessage.JSONProgress{
-				Current: update.Complete,
-				Total:   update.Total,
-			}).String())
-			if err != nil {
-				o.Log.Errorf("failed to write progress to console: %v", err)
-				return
-			}
-		}
-	}()
-
-	// write the image to the tarball file
-	err = tarball.Write(localRef, image, tarballFile)
-	if err != nil {
-		return fmt.Errorf("failed to write image to tarball: %w", err)
+	// get the filename of the image
+	fileName := strings.ReplaceAll(strings.ReplaceAll(srcRef.DockerReference().String(), "/", "_"), ":", "+") + ".tar"
+	if o.Destination != "" {
+		fileName = filepath.Join(o.Destination, fileName)
 	}
 
-	o.Log.Infof("Successfully pulled image %s", localRef.String())
+	// check if the image is a digest
+	isDigest := strings.Contains(image, "@")
+	imageListSelection := copy.CopySystemImage
+	if isDigest || o.Architecture == "all" {
+		imageListSelection = copy.CopyAllImages
+	} else {
+		srcContext.ArchitectureChoice = o.Architecture
+		destContext.ArchitectureChoice = o.Architecture
+	}
+
+	// this is always an oci-archive for now
+	destRef, err := alltransports.ParseImageName(fmt.Sprintf("oci-archive:%s", fileName))
+	if err != nil {
+		return fmt.Errorf("invalid destination name %s: %w", fileName, err)
+	}
+
+	// copy the image
+	o.Log.Infof("Pulling image %s to %s", srcRef.DockerReference().Name(), fileName)
+	_, err = copy.Image(ctx, destRef, srcRef, &copy.Options{
+		SourceCtx:      srcContext,
+		DestinationCtx: destContext,
+
+		PreserveDigests:    isDigest,
+		ImageListSelection: imageListSelection,
+
+		RemoveSignatures: true,
+
+		ReportWriter: os.Stdout,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to copy image: %w", err)
+	}
+
 	return nil
-}
-
-func refToFilename(ref name.Reference) string {
-	return strings.ReplaceAll(strings.ReplaceAll(ref.Name(), "/", "_"), ":", "_") + ".tar"
 }
