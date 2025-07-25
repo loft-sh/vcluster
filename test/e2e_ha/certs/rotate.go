43a79f2af9415b520157c38a76c1122101ab4948
Akhila Rachel Oommen <akhilarachel@Mac.fritz.box>
2025-07-25 13:12:52 +0200

    Added few e2e tests for vCluster cert rotation

---
+++ test/e2e_ha/certs/rotate.go
@@ -0,0 +1,156 @@
+package certs
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"time"
+
+	certscmd "github.com/loft-sh/vcluster/cmd/vclusterctl/cmd/certs"
+	"github.com/loft-sh/vcluster/pkg/certs"
+	"github.com/loft-sh/vcluster/pkg/cli/flags"
+	"github.com/loft-sh/vcluster/test/framework"
+	"github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var _ = ginkgo.Describe("vCluster cert rotation tests", ginkgo.Ordered, func() {
+	var (
+		f *framework.Framework
+	)
+
+	ginkgo.JustBeforeEach(func() {
+		f = framework.DefaultFramework
+	})
+
+	ginkgo.It("should obtain the current cert secret of vCluster", func() {
+		_, err := f.HostClient.CoreV1().Secrets(f.VClusterNamespace).Get(f.Context, certs.CertSecretName(f.VClusterName), metav1.GetOptions{})
+		framework.ExpectNoError(err)
+	})
+
+	ginkgo.It("Checking Expiry Date and time of vCluster CA cert", func() {
+		certsCmd := certscmd.NewCertsCmd(&flags.GlobalFlags{Namespace: f.VClusterNamespace})
+		certsCmd.SetArgs([]string{"check", f.VClusterName})
+
+		err := certsCmd.Execute()
+		framework.ExpectNoError(err)
+	})
+
+	ginkgo.It("Rotating CA cert of vCluster", func() {
+		certsCmd := certscmd.NewCertsCmd(&flags.GlobalFlags{Namespace: f.VClusterNamespace})
+		certsCmd.SetArgs([]string{"rotate-ca", f.VClusterName})
+
+		err := certsCmd.Execute()
+		framework.ExpectNoError(err)
+	})
+
+	ginkgo.It("should wait until the vCluster is ready again", func() {
+		framework.ExpectNoError(f.WaitForVClusterReady())
+		gomega.Eventually(func(g gomega.Gomega) error {
+			pods, err := f.HostClient.CoreV1().Pods(f.VClusterNamespace).List(f.Context, metav1.ListOptions{
+				LabelSelector: "app=vcluster,release=" + f.VClusterName,
+			})
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Expect(pods.Items).NotTo(gomega.BeEmpty())
+
+			for _, pod := range pods.Items {
+				gomega.Expect(pod.Status.ContainerStatuses).NotTo(gomega.BeEmpty(),
+					"pod %s should have container statuses", pod.Name)
+
+				for i, container := range pod.Status.ContainerStatuses {
+					gomega.Expect(container.State.Running).NotTo(gomega.BeNil(),
+						"container %d in pod %s should be running", i, pod.Name)
+					gomega.Expect(container.Ready).To(gomega.BeTrue(),
+						"container %d in pod %s should be ready", i, pod.Name)
+				}
+			}
+
+			return nil
+		}).WithPolling(time.Second).
+			WithTimeout(framework.PollTimeoutLong).
+			Should(gomega.Succeed())
+	})
+
+	ginkgo.It("Checking Expiry Date and time of vCluster CA cert", func() {
+		certsCmd := certscmd.NewCertsCmd(&flags.GlobalFlags{Namespace: f.VClusterNamespace})
+		certsCmd.SetArgs([]string{"check", f.VClusterName})
+
+		err := certsCmd.Execute()
+		framework.ExpectNoError(err)
+	})
+
+	ginkgo.It("Checking new validity date of CA cert of vCluster", func() {
+		secret, err := f.HostClient.CoreV1().Secrets(f.VClusterNamespace).Get(
+			f.Context, certs.CertSecretName(f.VClusterName), metav1.GetOptions{})
+		framework.ExpectNoError(err)
+
+		certPEM := secret.Data["ca.crt"]
+
+		block, _ := pem.Decode(certPEM)
+		gomega.Expect(block).NotTo(gomega.BeNil(), "Failed to decode PEM block")
+
+		cert, err := x509.ParseCertificate(block.Bytes)
+		framework.ExpectNoError(err)
+
+		gomega.Expect(cert.NotAfter.After(time.Now())).To(gomega.BeTrue(), "CA cert is valid")
+	})
+
+	ginkgo.It("Rotating Leaf cert of vCluster", func() {
+		certsCmd := certscmd.NewCertsCmd(&flags.GlobalFlags{Namespace: f.VClusterNamespace})
+		certsCmd.SetArgs([]string{"rotate", f.VClusterName})
+
+		err := certsCmd.Execute()
+		framework.ExpectNoError(err)
+	})
+
+	ginkgo.It("should wait until the vCluster is ready again", func() {
+		framework.ExpectNoError(f.WaitForVClusterReady())
+		gomega.Eventually(func(g gomega.Gomega) error {
+			pods, err := f.HostClient.CoreV1().Pods(f.VClusterNamespace).List(f.Context, metav1.ListOptions{
+				LabelSelector: "app=vcluster,release=" + f.VClusterName,
+			})
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+			gomega.Expect(pods.Items).NotTo(gomega.BeEmpty())
+
+			for _, pod := range pods.Items {
+				gomega.Expect(pod.Status.ContainerStatuses).NotTo(gomega.BeEmpty(),
+					"pod %s should have container statuses", pod.Name)
+
+				for i, container := range pod.Status.ContainerStatuses {
+					gomega.Expect(container.State.Running).NotTo(gomega.BeNil(),
+						"container %d in pod %s should be running", i, pod.Name)
+					gomega.Expect(container.Ready).To(gomega.BeTrue(),
+						"container %d in pod %s should be ready", i, pod.Name)
+				}
+			}
+
+			return nil
+		}).WithPolling(time.Second).
+			WithTimeout(framework.PollTimeoutLong).
+			Should(gomega.Succeed())
+	})
+
+	ginkgo.It("Checking Expiry Date and time of vCluster CA cert", func() {
+		certsCmd := certscmd.NewCertsCmd(&flags.GlobalFlags{Namespace: f.VClusterNamespace})
+		certsCmd.SetArgs([]string{"check", f.VClusterName})
+
+		err := certsCmd.Execute()
+		framework.ExpectNoError(err)
+	})
+
+	ginkgo.It("Checking new validity date of Leaf cert of vCluster", func() {
+		secret, err := f.HostClient.CoreV1().Secrets(f.VClusterNamespace).Get(
+			f.Context, certs.CertSecretName(f.VClusterName), metav1.GetOptions{})
+		framework.ExpectNoError(err)
+
+		certPEM := secret.Data["apiserver-etcd-client.crt"]
+
+		block, _ := pem.Decode(certPEM)
+		gomega.Expect(block).NotTo(gomega.BeNil(), "Failed to decode PEM block")
+
+		cert, err := x509.ParseCertificate(block.Bytes)
+		framework.ExpectNoError(err)
+
+		gomega.Expect(cert.NotAfter.After(time.Now())).To(gomega.BeTrue(), "Leaf cert is valid")
+	})
+})
