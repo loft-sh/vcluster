038fca114916751b3328b73a75c89ef5a55bbb7c
Russell Centanni <russell.centanni@gmail.com>
2025-07-28 13:50:13 -0400

    Snapshot List and Delete (#3021)
    * feat: implement vcluster snapshot list command
    
    * feat: implement vcluster snapshot delete command
    
    * fix: update snapshot validation for list command
    
    * fix: modify commands to error if the configuration cannot be parsed from the environment
    
    * fix: use full URL in list results
    
    * fix: only list objects in the "current directory" of the bucket
    
    The default s3 behavior is to list everything under a prefix. If a bucket is used for multiple purposes, this could include non-snapshot resources.
    
    * fix: handle list input with file key at the bucket root

--- cmd/vcluster/cmd/root.go
+++ cmd/vcluster/cmd/root.go
@@ -7,6 +7,7 @@ import (
 	"github.com/go-logr/logr"
 	loftlogr "github.com/loft-sh/log/logr"
 	"github.com/loft-sh/vcluster/cmd/vcluster/cmd/debug"
+	"github.com/loft-sh/vcluster/cmd/vcluster/cmd/snapshot"
 	"github.com/spf13/cobra"
 	"k8s.io/klog/v2"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -59,8 +60,8 @@ func BuildRoot() *cobra.Command {
 	// add top level commands
 	rootCmd.AddCommand(NewStartCommand())
 	rootCmd.AddCommand(NewCpCommand())
-	rootCmd.AddCommand(NewSnapshotCommand())
-	rootCmd.AddCommand(NewRestoreCommand())
+	rootCmd.AddCommand(snapshot.NewSnapshotCommand())
+	rootCmd.AddCommand(snapshot.NewRestoreCommand())
 	rootCmd.AddCommand(NewPortForwardCommand())
 	rootCmd.AddCommand(debug.NewDebugCmd())
 	return rootCmd
