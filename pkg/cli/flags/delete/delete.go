9f307263d94927e5f4d8897da33638daf417f6ba
Jan Baraniewski <dev@baraniewski.com>
2025-08-22 10:16:10 +0200

    Update description of delete-namespace flag

--- pkg/cli/flags/delete/delete.go
+++ pkg/cli/flags/delete/delete.go
@@ -21,7 +21,7 @@ func AddCommonFlags(cmd *cobra.Command, options *cli.DeleteOptions) {
 func AddHelmFlags(cmd *cobra.Command, options *cli.DeleteOptions) {
 	cmd.Flags().BoolVar(&options.DeleteConfigMap, "delete-configmap", false, "If enabled, vCluster will delete the ConfigMap of the vCluster")
 	cmd.Flags().BoolVar(&options.KeepPVC, "keep-pvc", false, "If enabled, vcluster will not delete the persistent volume claim of the vcluster")
-	cmd.Flags().BoolVar(&options.DeleteNamespace, "delete-namespace", false, "If enabled, vcluster will delete the namespace of the vcluster. In the case of multi-namespace mode, will also delete all other namespaces created by vcluster")
+	cmd.Flags().BoolVar(&options.DeleteNamespace, "delete-namespace", false, "If enabled, vCluster will delete the namespace of the vCluster. In cases when sync.toHost.namespaces was enabled, any namespaces and resources created due to syncing from the vCluster to the host cluster will also be deleted.")
 	cmd.Flags().BoolVar(&options.AutoDeleteNamespace, "auto-delete-namespace", true, "If enabled, vcluster will delete the namespace of the vcluster if it was created by vclusterctl. In the case of multi-namespace mode, will also delete all other namespaces created by vcluster")
 	cmd.Flags().BoolVar(&options.IgnoreNotFound, "ignore-not-found", false, "If enabled, vcluster will not error out in case the target vcluster does not exist")
 }
