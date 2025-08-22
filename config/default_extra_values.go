e083e08c7d7666e55ce591d4c5f2151481ba27ab
Amruta Kale <amruta.kale@loft.sh>
2025-08-22 13:40:34 +0530

    update k3s version

--- config/default_extra_values.go
+++ config/default_extra_values.go
@@ -23,7 +23,7 @@ const (
 
 // K3SVersionMap holds the supported k3s versions
 var K3SVersionMap = map[string]string{
-	"1.33": "rancher/k3s:v1.33.4-k3s1",
+	"1.33": "rancher/k3s:v1.33.3-k3s1",
 	"1.32": "rancher/k3s:v1.32.1-k3s1",
 	"1.31": "rancher/k3s:v1.31.1-k3s1",
 	"1.30": "rancher/k3s:v1.30.2-k3s1",
