ef42ade0713cd6708c4bc540b98994206e03fe3e
Nikola Prokopić <nikola.prokopic@loft.sh>
2025-09-16 22:27:27 +0200

    Delete empty file

--- pkg/snapshot/volumes/csi/snapshotter.go
+++
@@ -1,6 +0,0 @@
-package csi
-
-import (
-	_ "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumesnapshot/v1"
-	_ "github.com/kubernetes-csi/external-snapshotter/client/v8/clientset/versioned"
-)
