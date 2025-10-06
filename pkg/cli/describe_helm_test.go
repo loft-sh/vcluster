73608db0f76f67ecd756faadb882a3a12ef23934
Catalin Balan <catalin.balan@loft.sh>
2025-10-06 21:36:53 +0200

    fix: Added left behind tests

---
+++ pkg/cli/describe_helm_test.go
@@ -0,0 +1,296 @@
+package cli
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/aws/smithy-go/ptr"
+	"github.com/loft-sh/vcluster/config"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestDescribeOutputString(t *testing.T) {
+	tests := []struct {
+		name string
+		do   DescribeOutput
+		want string
+	}{
+		{
+			name: "empty user supplied vcluster.yaml",
+			do: DescribeOutput{
+				Name:         "test",
+				Namespace:    "vcluster-test",
+				Version:      "0.29.0",
+				BackingStore: string(config.StoreTypeEmbeddedDatabase),
+				Distro:       config.K8SDistro,
+				Status:       "Running",
+				Created:      metav1.NewTime(time.Unix(1759769661, 0)),
+				Images: map[string]string{
+					"apiServer": "ghcr.io/loft-sh/kubernetes:v1.33.4",
+					"syncer":    "ghcr.io/loft-sh/vcluster-pro:0.29.0",
+				},
+			},
+			want: `Name:           test
+Namespace:      vcluster-test
+Version:        0.29.0
+Backing Store:  embedded-database
+Distro:         k8s
+Created:        Mon, 06 Oct 2025 18:54:21 +0200
+Status:         Running
+Images:
+  apiServer:  ghcr.io/loft-sh/kubernetes:v1.33.4
+  syncer:     ghcr.io/loft-sh/vcluster-pro:0.29.0
+`},
+		{
+			name: "user supplied vcluster.yaml",
+			do: DescribeOutput{
+				Name:         "test",
+				Namespace:    "vcluster-test",
+				Version:      "0.29.0",
+				BackingStore: string(config.StoreTypeEmbeddedDatabase),
+				Distro:       config.K8SDistro,
+				Status:       "Running",
+				Created:      metav1.NewTime(time.Unix(1759769661, 0)),
+				Images: map[string]string{
+					"apiServer": "ghcr.io/loft-sh/kubernetes:v1.33.4",
+					"syncer":    "ghcr.io/loft-sh/vcluster-pro:0.29.0",
+				},
+				UserConfigYaml: ptr.String(`sync:
+  toHost:
+    serviceAccounts:
+      enabled: true
+`),
+			},
+			want: `Name:           test
+Namespace:      vcluster-test
+Version:        0.29.0
+Backing Store:  embedded-database
+Distro:         k8s
+Created:        Mon, 06 Oct 2025 18:54:21 +0200
+Status:         Running
+Images:
+  apiServer:  ghcr.io/loft-sh/kubernetes:v1.33.4
+  syncer:     ghcr.io/loft-sh/vcluster-pro:0.29.0
+
+------------------- vcluster.yaml -------------------
+sync:
+  toHost:
+    serviceAccounts:
+      enabled: true
+-----------------------------------------------------
+Use --config-only to retrieve just the vcluster.yaml
+`,
+		},
+		{
+			name: "truncated user supplied vcluster.yaml",
+			do: DescribeOutput{
+				Name:         "test",
+				Namespace:    "vcluster-test",
+				Version:      "0.29.0",
+				BackingStore: string(config.StoreTypeEmbeddedDatabase),
+				Distro:       config.K8SDistro,
+				Status:       "Running",
+				Created:      metav1.NewTime(time.Unix(1759769661, 0)),
+				Images: map[string]string{
+					"apiServer": "ghcr.io/loft-sh/kubernetes:v1.33.4",
+					"syncer":    "ghcr.io/loft-sh/vcluster-pro:0.29.0",
+				},
+				UserConfigYaml: ptr.String(strings.Repeat("line\n", 51)),
+			},
+			want: fmt.Sprintf(`Name:           test
+Namespace:      vcluster-test
+Version:        0.29.0
+Backing Store:  embedded-database
+Distro:         k8s
+Created:        Mon, 06 Oct 2025 18:54:21 +0200
+Status:         Running
+Images:
+  apiServer:  ghcr.io/loft-sh/kubernetes:v1.33.4
+  syncer:     ghcr.io/loft-sh/vcluster-pro:0.29.0
+
+------------------- vcluster.yaml -------------------
+%s... (truncated)
+-----------------------------------------------------
+Use --config-only to retrieve the full vcluster.yaml only
+`, strings.Repeat("line\n", 50)),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.do.String(); got != tt.want {
+				t.Errorf("String() = got %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestConfigPartialUnmarshal(t *testing.T) {
+	type args struct {
+		configBytes []byte
+	}
+	tests := []struct {
+		name                 string
+		args                 args
+		want                 *config.Config
+		wantErr              bool
+		wantDistro           string
+		wantBackingStoreType config.StoreType
+	}{
+		{
+			name: "empty content",
+			args: args{
+				configBytes: []byte(""),
+			},
+			want:                 &config.Config{},
+			wantErr:              false,
+			wantDistro:           config.K8SDistro,
+			wantBackingStoreType: config.StoreTypeEmbeddedDatabase,
+		},
+		{
+			name: "parse only the controlPlane section",
+			args: args{[]byte(`
+controlPlane:
+  advanced:
+    defaultImageRegistry: ghcr.io
+  distro:
+    k8s:
+      apiServer:
+        enabled: true
+      controllerManager:
+        enabled: true
+      image:
+        registry: ghcr.io
+        repository: loft-sh/kubernetes
+        tag: v1.33.4
+  statefulSet:
+    image:
+      registry: ghcr.io
+      repository: loft-sh/vcluster-pro
+telemetry:
+  enabled: true
+
+fieldNotMatchingConfigSchema:
+  this: ["should", "be", "ignored"]
+`)},
+			want: &config.Config{
+				ControlPlane: config.ControlPlane{
+					Advanced: config.ControlPlaneAdvanced{
+						DefaultImageRegistry: "ghcr.io",
+					},
+					Distro: config.Distro{
+						K8S: config.DistroK8s{
+							APIServer: config.DistroContainerEnabled{
+								Enabled: true,
+							},
+							ControllerManager: config.DistroContainerEnabled{
+								Enabled: true,
+							},
+							DistroCommon: config.DistroCommon{
+								Image: config.Image{
+									Registry:   "ghcr.io",
+									Repository: "loft-sh/kubernetes",
+									Tag:        "v1.33.4",
+								},
+							},
+						},
+					},
+					StatefulSet: config.ControlPlaneStatefulSet{
+						Image: config.Image{
+							Registry:   "ghcr.io",
+							Repository: "loft-sh/vcluster-pro",
+						},
+					},
+				},
+
+				// Telemetry section is expected to not be parsed.
+				Telemetry: config.Telemetry{
+					Enabled: false,
+				},
+			},
+			wantErr:              false,
+			wantBackingStoreType: config.StoreTypeEmbeddedDatabase,
+			wantDistro:           config.K8SDistro,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := configPartialUnmarshal(tt.args.configBytes)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("configPartialUnmarshal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("configPartialUnmarshal() got = %v, want %v", got, tt.want)
+			}
+
+			if gotDistro := got.Distro(); gotDistro != tt.wantDistro {
+				t.Errorf("distro got = %v, want %v", gotDistro, tt.wantDistro)
+			}
+
+			if gotBackingStoreType := got.BackingStoreType(); gotBackingStoreType != tt.wantBackingStoreType {
+				t.Errorf("backingStoreType got = %v, want %v", gotBackingStoreType, tt.wantBackingStoreType)
+			}
+		})
+	}
+}
+
+func TestGetImagesFromConfig(t *testing.T) {
+	type args struct {
+		c       *config.Config
+		version string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[string]string
+	}{
+		{
+			name: "empty config",
+			args: args{&config.Config{}, "0.29.0"},
+			want: map[string]string{
+				"syncer": "ghcr.io/loft-sh/library/vcluster-pro:0.29.0",
+			},
+		},
+		{
+			name: "syncer from config",
+			args: args{&config.Config{
+				ControlPlane: config.ControlPlane{
+					Distro: config.Distro{
+						K8S: config.DistroK8s{
+							DistroCommon: config.DistroCommon{
+								Image: config.Image{
+									Registry:   "ghcr.io",
+									Repository: "loft-sh/kubernetes",
+									Tag:        "v1.33.4",
+								},
+							},
+						},
+					},
+					StatefulSet: config.ControlPlaneStatefulSet{
+						Image: config.Image{
+							Registry:   "ghcr.io",
+							Repository: "loft-sh/vcluster-pro",
+							Tag:        "0.29.0",
+						},
+					},
+				},
+			}, "v1.33.4"},
+			want: map[string]string{
+				"apiServer": "ghcr.io/loft-sh/kubernetes:v1.33.4",
+				"syncer":    "ghcr.io/loft-sh/vcluster-pro:0.29.0",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getImagesFromConfig(tt.args.c, tt.args.version); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getImagesFromConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
