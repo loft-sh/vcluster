d65397317cb083a79bf7fc01a8a5403afbfaa363
Nikola Prokopić <nikola.prokopic@loft.sh>
2025-10-23 15:05:20 +0200

    Fix getting volumes to snapshot

--- pkg/snapshot/volumes/csi/snapshotnotstarted.go
+++ pkg/snapshot/volumes/csi/snapshotnotstarted.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/loft-sh/vcluster/pkg/snapshot/volumes"
+	"github.com/loft-sh/vcluster/pkg/util/translate"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -16,28 +17,42 @@ func (s *VolumeSnapshotter) reconcileNotStarted(ctx context.Context, requestName
 	}
 	defer s.logger.Infof("Reconciled new volume snapshots request %s", requestName)
 
-	// first get all persistent volumes
-	pvs, err := s.kubeClient.CoreV1().PersistentVolumes().List(ctx, v1.ListOptions{})
+	// first get all PersistentVolumeClaims
+	// for private nodes, we need to get all PVCs in the virtual cluster
+	// for share nodes, we need to get only PVCs in the vCluster host namespace
+	var pvcsNamespace string
+	if !s.vConfig.PrivateNodes.Enabled {
+		// if private nodes are disabled, we need to get all PVCs in all namespaces
+		pvcsNamespace = s.vConfig.HostNamespace
+	}
+	listOptions := v1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=%s", translate.MarkerLabel, s.vConfig.Name),
+	}
+	pvcs, err := s.kubeClient.CoreV1().PersistentVolumeClaims(pvcsNamespace).List(ctx, listOptions)
 	if err != nil {
-		return fmt.Errorf("failed to list persistent volumes: %w", err)
+		return fmt.Errorf("failed to list PersistentVolumeClaims: %w", err)
 	}
 
 	var volumeSnapshotRequests []volumes.SnapshotRequest
-	for _, pv := range pvs.Items {
+	for _, pvc := range pvcs.Items {
+		if pvc.Spec.VolumeName == "" {
+			// PVC is not bound to a PV, skip it
+			continue
+		}
+		pv, err := s.kubeClient.CoreV1().PersistentVolumes().Get(ctx, pvc.Spec.VolumeName, v1.GetOptions{})
+		if err != nil {
+			s.logger.Errorf("failed to get PersistentVolume %s for PersistentVolumeClaim %s/%s: %w", pvc.Spec.VolumeName, pvc.Namespace, pvc.Name, err)
+			continue
+		}
+
 		// check if creating a snapshot for this PV is supported
-		err = s.CheckIfPersistentVolumeIsSupported(&pv)
+		err = s.CheckIfPersistentVolumeIsSupported(pv)
 		if err != nil {
 			s.logger.Infof("Skip creating a snapshot for PersistentVolume %s, since it is not supported: %v", pv.Name, err)
 			continue
 		}
 		volumeSnapshotRequest := volumes.SnapshotRequest{
 			CSIDriver: pv.Spec.CSI.Driver,
-		}
-
-		pvc, err := s.kubeClient.CoreV1().PersistentVolumeClaims(pv.Spec.ClaimRef.Namespace).Get(ctx, pv.Spec.ClaimRef.Name, v1.GetOptions{})
-		if err != nil {
-			s.logger.Errorf("failed to get PersistentVolumeClaim %s/%s for PersistentVolume %s: %w", pv.Spec.ClaimRef.Namespace, pv.Spec.ClaimRef.Name, pv.Name, err)
-			continue
 		}
 		pvcCopy := pvc.DeepCopy()
 		delete(pvcCopy.Annotations, "kubectl.kubernetes.io/last-applied-configuration")
