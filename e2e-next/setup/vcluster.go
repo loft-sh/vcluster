a42310a8e8d63ce0ef6b5aeccabec03c96999fa5
Adrian <adrian.kabala@loft.sh>
2025-11-03 13:04:28 +0100

    chore(tests): vcluster setup upgrade to use ctx

--- e2e-next/setup/vcluster.go
+++ e2e-next/setup/vcluster.go
@@ -7,9 +7,8 @@ import (
 	"path/filepath"
 
 	"github.com/loft-sh/e2e-framework/pkg/e2e"
-	"github.com/loft-sh/e2e-framework/pkg/setup/cluster"
+	"github.com/loft-sh/e2e-framework/pkg/setup"
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/e2e-framework/klient"
@@ -17,121 +16,268 @@ import (
 	"sigs.k8s.io/e2e-framework/third_party/vcluster"
 )
 
-// VClusterSetup contains all the clients and configuration for a vcluster test
-type VClusterSetup struct {
-	Cluster        *vcluster.Cluster
-	RestConfig     *rest.Config
-	KlientClient   klient.Client
-	KubeClient     kubernetes.Interface
-	ValuesFilePath string
+type key int
+
+const (
+	vclusterNameContextKey key = iota
+	vclusterClusterContextKey
+	vclusterRestConfigContextKey
+	vclusterKlientClientContextKey
+	vclusterKubeClientContextKey
+)
+
+type vclusterContextKey string
+
+func With(ctx context.Context, name string, vclusterCluster *vcluster.Cluster) context.Context {
+	return context.WithValue(ctx, vclusterContextKey(name), vclusterCluster)
 }
 
-func CreateVClusterFromValues(ctx context.Context, vclusterName string, valuesYAML string) *VClusterSetup {
-	setup := &VClusterSetup{}
-
-	// Write values to a temporary file
-	valuesFile := setup.createTempValuesFile(vclusterName, valuesYAML)
-
-	// Clean up the temporary file
-	e2e.DeferCleanup(func(ctx context.Context) {
-		By("Removing temp vcluster.yaml file")
-		_ = os.Remove(valuesFile)
-	})
-
-	// Create vcluster instance
-	setup.Cluster = vcluster.NewCluster(vclusterName)
-
-	// Create the vcluster with the values file
-	_, err := setup.Cluster.CreateWithConfig(ctx, valuesFile)
-	Expect(err).NotTo(HaveOccurred(), "Failed to create vcluster")
-
-	// Get the kubeconfig and create clients
-	setup.RestConfig = GetRestConfig(setup.Cluster)
-	setup.KlientClient = GetKlientClient(setup.RestConfig)
-	setup.KubeClient = GetKubernetesClient(setup.RestConfig)
-
-	// Set current cluster in context
-	cluster.SetCurrentCluster(vclusterName)(ctx)
-
-	// Remove vcluster after test
-	e2e.DeferCleanup(func(ctx context.Context) {
-		By("Removing vcluster")
-		_ = setup.Cluster.Destroy(ctx)
-	})
-	return setup
+func From(ctx context.Context, name string) *vcluster.Cluster {
+	if k, ok := ctx.Value(vclusterContextKey(name)).(*vcluster.Cluster); ok {
+		return k
+	}
+	return nil
 }
 
-func (s *VClusterSetup) createTempValuesFile(vclusterName string, valuesYAML string) string {
+func WithVClusterName(ctx context.Context, name string) context.Context {
+	return context.WithValue(ctx, vclusterNameContextKey, name)
+}
+
+func VClusterNameFrom(ctx context.Context) string {
+	if value := ctx.Value(vclusterNameContextKey); value != nil {
+		return value.(string)
+	}
+	return ""
+}
+
+func WithVClusterCluster(ctx context.Context, vclusterCluster *vcluster.Cluster) context.Context {
+	return context.WithValue(ctx, vclusterClusterContextKey, vclusterCluster)
+}
+
+func VClusterClusterFrom(ctx context.Context) *vcluster.Cluster {
+	if value := ctx.Value(vclusterClusterContextKey); value != nil {
+		return value.(*vcluster.Cluster)
+	}
+	return nil
+}
+
+func WithVClusterRestConfig(ctx context.Context, restConfig *rest.Config) context.Context {
+	return context.WithValue(ctx, vclusterRestConfigContextKey, restConfig)
+}
+
+func VClusterRestConfigFrom(ctx context.Context) *rest.Config {
+	if value := ctx.Value(vclusterRestConfigContextKey); value != nil {
+		return value.(*rest.Config)
+	}
+	return nil
+}
+
+func WithVClusterKlientClient(ctx context.Context, klientClient klient.Client) context.Context {
+	return context.WithValue(ctx, vclusterKlientClientContextKey, klientClient)
+}
+
+func VClusterKlientClientFrom(ctx context.Context) klient.Client {
+	if value := ctx.Value(vclusterKlientClientContextKey); value != nil {
+		return value.(klient.Client)
+	}
+	return nil
+}
+
+func WithVClusterKubeClient(ctx context.Context, kubeClient kubernetes.Interface) context.Context {
+	return context.WithValue(ctx, vclusterKubeClientContextKey, kubeClient)
+}
+
+func GetKubeClientFrom(ctx context.Context) kubernetes.Interface {
+	if value := ctx.Value(vclusterKubeClientContextKey); value != nil {
+		return value.(kubernetes.Interface)
+	}
+	return nil
+}
+
+type vclusterOptions struct {
+	name       string
+	valuesYAML string
+	valuesFile string
+}
+
+type VClusterOptions func(*vclusterOptions)
+
+func WithName(name string) VClusterOptions {
+	return func(o *vclusterOptions) {
+		o.name = name
+	}
+}
+
+func WithValuesYAML(valuesYAML string) VClusterOptions {
+	return func(o *vclusterOptions) {
+		o.valuesYAML = valuesYAML
+	}
+}
+
+func WithValuesFile(valuesFile string) VClusterOptions {
+	return func(o *vclusterOptions) {
+		o.valuesFile = valuesFile
+	}
+}
+
+func Create(opts ...VClusterOptions) setup.Func {
+	return func(ctx context.Context) (context.Context, error) {
+		o := &vclusterOptions{}
+		for _, opt := range opts {
+			opt(o)
+		}
+
+		if o.name == "" {
+			return ctx, fmt.Errorf("vcluster name is required")
+		}
+
+		// Check if vcluster already exists in context
+		existingVCluster := From(ctx, o.name)
+		if existingVCluster != nil {
+			By("Reusing existing vcluster: " + o.name)
+			return ctx, nil
+		}
+
+		var valuesFile string
+		var err error
+
+		if o.valuesYAML != "" {
+			valuesFile, err = createTempValuesFile(o.name, o.valuesYAML)
+			if err != nil {
+				return ctx, fmt.Errorf("failed to create temp values file: %w", err)
+			}
+
+			// Clean up the temporary file
+			e2e.DeferCleanup(func(ctx context.Context) {
+				By("Removing temp vcluster.yaml file")
+				_ = os.Remove(valuesFile)
+			})
+		} else if o.valuesFile != "" {
+			valuesFile = o.valuesFile
+		} else {
+			return ctx, fmt.Errorf("either valuesYAML or valuesFile must be provided")
+		}
+		// Create vcluster instance
+		vclusterCluster := vcluster.NewCluster(o.name)
+		// Create the vcluster with the values file
+		By("Creating vcluster")
+		_, err = vclusterCluster.CreateWithConfig(ctx, valuesFile)
+		if err != nil {
+			return ctx, fmt.Errorf("failed to create vcluster: %w", err)
+		}
+		// Get the kubeconfig and create clients
+		restConfig, err := setupRestConfig(vclusterCluster)
+		if err != nil {
+			return ctx, fmt.Errorf("failed to get rest config: %w", err)
+		}
+		klientClient, err := setupKlientClient(restConfig)
+		if err != nil {
+			return ctx, fmt.Errorf("failed to get klient client: %w", err)
+		}
+		kubeClient, err := setupKubeClient(restConfig)
+		if err != nil {
+			return ctx, fmt.Errorf("failed to get kubernetes client: %w", err)
+		}
+		// Store vcluster data in context
+		ctx = With(ctx, o.name, vclusterCluster)
+		ctx = WithVClusterName(ctx, o.name)
+		ctx = WithVClusterCluster(ctx, vclusterCluster)
+		ctx = WithVClusterRestConfig(ctx, restConfig)
+		ctx = WithVClusterKlientClient(ctx, klientClient)
+		ctx = WithVClusterKubeClient(ctx, kubeClient)
+
+		// Remove vcluster after test
+		e2e.DeferCleanup(func(ctx context.Context) {
+			By("Removing vcluster")
+			_ = vclusterCluster.Destroy(ctx)
+		})
+
+		return ctx, nil
+	}
+}
+
+func Destroy(vclusterName string) setup.Func {
+	return func(ctx context.Context) (context.Context, error) {
+		if vclusterName == "" {
+			return ctx, fmt.Errorf("vcluster name is required")
+		}
+
+		vclusterCluster := From(ctx, vclusterName)
+		if vclusterCluster == nil {
+			return ctx, fmt.Errorf("vcluster %s not found in context", vclusterName)
+		}
+
+		if err := vclusterCluster.Destroy(ctx); err != nil {
+			return ctx, fmt.Errorf("failed to destroy vcluster: %w", err)
+		}
+
+		return ctx, nil
+	}
+}
+
+func WaitForControlPlane(ctx context.Context) error {
+	vclusterCluster := VClusterClusterFrom(ctx)
+	if vclusterCluster == nil {
+		return fmt.Errorf("vcluster cluster not found in context")
+	}
+
+	klientClient := VClusterKlientClientFrom(ctx)
+	if klientClient == nil {
+		return fmt.Errorf("vcluster klient client not found in context")
+	}
+
+	By("Waiting for vcluster control plane to be ready")
+	return vclusterCluster.WaitForControlPlane(ctx, klientClient)
+}
+
+func createTempValuesFile(vclusterName string, valuesYAML string) (string, error) {
 	By("Creating temp vcluster.yaml file")
 	tmpDir := os.TempDir()
 	valuesFile := filepath.Join(tmpDir, fmt.Sprintf("vcluster-values-%s.yaml", vclusterName))
 	err := os.WriteFile(valuesFile, []byte(valuesYAML), 0644)
-	Expect(err).NotTo(HaveOccurred(), "Failed to write vcluster values file")
-	s.ValuesFilePath = valuesFile
-	return valuesFile
+	if err != nil {
+		return "", fmt.Errorf("failed to write vcluster values file: %w", err)
+	}
+	return valuesFile, nil
 }
 
-// CreateVClusterFromFile creates a vcluster using a YAML values file path
-func CreateVClusterFromFile(ctx context.Context, vclusterName string, valuesFilePath string) *VClusterSetup {
-	setup := &VClusterSetup{}
-
-	// Create vcluster instance
-	setup.Cluster = vcluster.NewCluster(vclusterName)
-
-	// Create the vcluster with the values file
-	By("Creating vcluster")
-	_, err := setup.Cluster.CreateWithConfig(ctx, valuesFilePath)
-	Expect(err).NotTo(HaveOccurred(), "Failed to create vcluster")
-
-	// Get the kubeconfig and create clients
-	setup.RestConfig = GetRestConfig(setup.Cluster)
-	setup.KlientClient = GetKlientClient(setup.RestConfig)
-	setup.KubeClient = GetKubernetesClient(setup.RestConfig)
-
-	// Set current cluster in context
-	cluster.SetCurrentCluster(vclusterName)(ctx)
-
-	// Setup cleanup
-	e2e.DeferCleanup(func(ctx context.Context) {
-		_ = setup.Cluster.Destroy(ctx)
-	})
-
-	return setup
-}
-
-// WaitForControlPlane waits for the vcluster control plane to be ready
-func (s *VClusterSetup) WaitForControlPlane(ctx context.Context) {
-	By("Waiting for vcluster control plane to be ready")
-	err := s.Cluster.WaitForControlPlane(ctx, s.KlientClient)
-	Expect(err).NotTo(HaveOccurred(), "Failed to wait for vcluster control plane")
-}
-
-// GetRestConfig creates a rest.Config from a vcluster Cluster
-func GetRestConfig(vclusterCluster *vcluster.Cluster) *rest.Config {
+func setupRestConfig(vclusterCluster *vcluster.Cluster) (*rest.Config, error) {
 	kubeconfigPath := vclusterCluster.GetKubeconfig()
-	Expect(kubeconfigPath).NotTo(BeEmpty(), "vcluster kubeconfig path should not be empty")
+	if kubeconfigPath == "" {
+		return nil, fmt.Errorf("vcluster kubeconfig path should not be empty")
+	}
 
 	restConfig, err := conf.New(kubeconfigPath)
-	Expect(err).NotTo(HaveOccurred(), "Failed to create vcluster rest config")
-	Expect(restConfig).NotTo(BeNil(), "vcluster config should not be nil")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create vcluster rest config: %w", err)
+	}
+	if restConfig == nil {
+		return nil, fmt.Errorf("vcluster config should not be nil")
+	}
 
-	return restConfig
+	return restConfig, nil
 }
 
-// GetKlientClient creates a klient.Client from a rest.Config
-func GetKlientClient(restConfig *rest.Config) klient.Client {
+func setupKlientClient(restConfig *rest.Config) (klient.Client, error) {
 	klientClient, err := klient.New(restConfig)
-	Expect(err).NotTo(HaveOccurred(), "Failed to create klient client")
-	Expect(klientClient).NotTo(BeNil(), "klient client should not be nil")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create klient client: %w", err)
+	}
+	if klientClient == nil {
+		return nil, fmt.Errorf("klient client should not be nil")
+	}
 
-	return klientClient
+	return klientClient, nil
 }
 
-// GetKubernetesClient creates a kubernetes.Interface from a rest.Config
-func GetKubernetesClient(restConfig *rest.Config) kubernetes.Interface {
+func setupKubeClient(restConfig *rest.Config) (kubernetes.Interface, error) {
 	kubeClient, err := kubernetes.NewForConfig(restConfig)
-	Expect(err).NotTo(HaveOccurred(), "Failed to create kubernetes client")
-	Expect(kubeClient).NotTo(BeNil(), "kubernetes client should not be nil")
+	if err != nil {
+		return nil, fmt.Errorf("failed to create kubernetes client: %w", err)
+	}
+	if kubeClient == nil {
+		return nil, fmt.Errorf("kubernetes client should not be nil")
+	}
 
-	return kubeClient
+	return kubeClient, nil
 }
