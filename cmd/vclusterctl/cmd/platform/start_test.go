caf3514d5d3445d0879bace52752a9a36cb8d468
Nikola Prokopić <nikola.prokopic@loft.sh>
2025-12-04 16:05:18 +0100

    Add unit tests

---
+++ cmd/vclusterctl/cmd/platform/start_test.go
@@ -0,0 +1,43 @@
+package platform
+
+import (
+	"testing"
+
+	"github.com/loft-sh/log"
+	"github.com/loft-sh/vcluster/pkg/cli/flags"
+	"github.com/loft-sh/vcluster/pkg/cli/start"
+)
+
+func TestPlatformUsesNewActivationFlow(t *testing.T) {
+	testCases := []struct {
+		version  string
+		expected bool
+	}{
+		{"4.5.0", false},
+		{"4.5.1", false},
+		{"4.6.0-alpha.5", false},
+		{"4.6.0-next.2", false},
+		{"4.6.0-next.internal.0", false},
+		{"4.6.0-next.internal.1", true},
+		{"4.6.0-next.internal.2", true},
+		{"4.6.0", true},
+	}
+
+	globalFlags := &flags.GlobalFlags{}
+	startCmd := &StartCmd{
+		StartOptions: start.StartOptions{
+			Options: start.Options{
+				CommandName: "start",
+				GlobalFlags: globalFlags,
+				Log:         log.GetInstance(),
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		result := startCmd.platformUsesNewActivationFlow(testCase.version)
+		if result != testCase.expected {
+			t.Errorf("Expected %v, got %v for platform version %s", testCase.expected, result, testCase.version)
+		}
+	}
+}
