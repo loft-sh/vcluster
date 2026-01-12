f043d650a2cac5eeb98e2c2ecd3d5f29387f8190
rlmcpherson <rlmcpherson@gmail.com>
2026-01-12 14:46:16 -0700

    fix lint error

--- pkg/pro/error.go
+++ pkg/pro/error.go
@@ -11,5 +11,5 @@ func NewFeatureError(featureName string) error {
 	if err != nil {
 		displayName = "Unknown Feature"
 	}
-	return fmt.Errorf("you are trying to use a vCluster pro feature %s (%s) that is not part of the open-source build of vCluster. Please use the vCluster pro image and specify a license that allows using this feature or reach out to support@loft.sh", &displayName, featureName)
+	return fmt.Errorf("you are trying to use a vCluster pro feature %s (%s) that is not part of the open-source build of vCluster. Please use the vCluster pro image and specify a license that allows using this feature or reach out to support@loft.sh", displayName, featureName)
 }
