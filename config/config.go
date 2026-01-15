d047576d44191a1e49fb2b44b4a84c18d11b0e5e
Fabian Kramm <fab.kramm@googlemail.com>
2026-01-15 16:28:39 +0100

    fix: pro feature enabled check

--- config/config.go
+++ config/config.go
@@ -1078,7 +1078,8 @@ func (c *Config) IsProFeatureEnabled() bool {
 		return true
 	}
 
-	if c.PrivateNodes.Enabled {
+	// private nodes is allowed in standalone mode
+	if c.PrivateNodes.Enabled && !c.ControlPlane.Standalone.Enabled {
 		return true
 	}
 
