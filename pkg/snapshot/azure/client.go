3561db9f5ddfe2f24a54c1989c5433ab94251711
Nikola Prokopić <nikola.prokopic@loft.sh>
2026-02-13 17:12:05 +0100

    Use default credentials when SAS is not set

--- pkg/snapshot/azure/client.go
+++ pkg/snapshot/azure/client.go
@@ -5,6 +5,7 @@ import (
 	"net/url"
 	"strings"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/blockblob"
 	"github.com/Azure/azure-sdk-for-go/sdk/storage/azblob/container"
 )
@@ -44,11 +45,41 @@ func GetBlobInfo(blobURL string) (BlobInfo, error) {
 }
 
 // NewBlobClient creates an Azure blob client from BlobInfo and a full blob URL with SAS token
-func NewBlobClient(info BlobInfo, blobURL string) (*blockblob.Client, error) {
-	// Create the block blob client with SAS token (no credentials needed, token is in URL)
-	blobClient, err := blockblob.NewClientWithNoCredential(blobURL, nil)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create blob client: %w", err)
+func NewBlobClient(info BlobInfo, blobURL string, useDefaultCredentials bool) (*blockblob.Client, error) {
+	var blobClient *blockblob.Client
+	var err error
+	if useDefaultCredentials {
+		// TODO use default Azure credentials to create blob client
+		// Use default Azure credentials for authentication. From Azure SDK go docs:
+		//
+		// DefaultAzureCredential attempts to authenticate with each of these credential types, in the following order,
+		// stopping when one provides a token:
+		//    1. EnvironmentCredential
+		//    2. WorkloadIdentityCredential, if environment variable configuration is set by the Azure workload identity webhook.
+		//    3. ManagedIdentityCredential
+		//    4. AzureCLICredential
+		//    5. AzureDeveloperCLICredential
+		//    6. AzurePowerShellCredential
+		//
+		// More details in go docs here https://pkg.go.dev/github.com/Azure/azure-sdk-for-go/sdk/azidentity#DefaultAzureCredential.
+		//
+		// TODO: if using default Azure credentials does not work here, then try share key approach that requires using storage key
+		//
+		var defaultCredential *azidentity.DefaultAzureCredential
+		defaultCredential, err = azidentity.NewDefaultAzureCredential(nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create default Azure credential: %w", err)
+		}
+		blobClient, err = blockblob.NewClient(blobURL, defaultCredential, nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create blob client with default Azure credentials: %w", err)
+		}
+	} else {
+		// Create the block blob client with SAS token (no credentials needed, token is in URL)
+		blobClient, err = blockblob.NewClientWithNoCredential(blobURL, nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create blob client with SAS token: %w", err)
+		}
 	}
 
 	return blobClient, nil
