c1ef4e9832b8585b1e8cc7ae3581301a2f3503a7
Adrian <adrian.kabala@loft.sh>
2026-03-10 11:45:01 +0100

    chore(e2e-next): remove servicesync tests

--- test/e2e_scheduler/e2e_scheduler_suite_test.go
+++ test/e2e_scheduler/e2e_scheduler_suite_test.go
@@ -17,7 +17,6 @@ import (
 	_ "github.com/loft-sh/vcluster/test/e2e/coredns"
 	_ "github.com/loft-sh/vcluster/test/e2e/k8sdefaultendpoint"
 	_ "github.com/loft-sh/vcluster/test/e2e/manifests"
-	_ "github.com/loft-sh/vcluster/test/e2e/servicesync"
 	_ "github.com/loft-sh/vcluster/test/e2e/snapshot"
 	_ "github.com/loft-sh/vcluster/test/e2e/syncer/networkpolicies"
 	_ "github.com/loft-sh/vcluster/test/e2e/syncer/pods"
