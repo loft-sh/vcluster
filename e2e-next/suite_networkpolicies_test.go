aa6c280fbb60898d29b6de4405a2e9b98884336a
Adrian <adrian.kabala@loft.sh>
2026-04-09 09:56:35 +0200

    chore(e2e-next): add more tests to networkpolicies suite

--- e2e-next/suite_networkpolicies_test.go
+++ e2e-next/suite_networkpolicies_test.go
@@ -24,7 +24,12 @@ package e2e_next
 import (
 	"github.com/loft-sh/e2e-framework/pkg/setup/cluster"
 	"github.com/loft-sh/vcluster/e2e-next/clusters"
+	"github.com/loft-sh/vcluster/e2e-next/labels"
+	"github.com/loft-sh/vcluster/e2e-next/test_core/coredns"
 	test_core "github.com/loft-sh/vcluster/e2e-next/test_core/sync"
+	"github.com/loft-sh/vcluster/e2e-next/test_core/sync/fromhost"
+	"github.com/loft-sh/vcluster/e2e-next/test_deploy"
+	"github.com/loft-sh/vcluster/e2e-next/test_security/webhook"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -34,10 +39,24 @@ func init() {
 
 func suiteNetworkPoliciesVCluster() {
 	Describe("networkpolicies-vcluster",
+		labels.NetworkPolicies, labels.NonDefault,
 		cluster.Use(clusters.CommonVCluster),
 		cluster.Use(clusters.HostCluster),
 		func() {
 			test_core.NetworkPolicyEnforcementSpec()
+			test_core.PodSyncSpec()
+			test_core.NetworkPolicySyncSpec()
+			test_core.PVCSyncSpec()
+			test_core.K8sDefaultEndpointSpec()
+			test_core.NodeSyncLabelSelectorSpec()
+			test_core.ServiceBasicSyncSpec()
+			coredns.CoreDNSSpec()
+			webhook.AdmissionWebhookSpec()
+			fromhost.EventSyncSpec()
+			fromhost.FromHostConfigMapsSpec()
+			fromhost.FromHostSecretsSpec()
+			test_deploy.HelmChartsSpec()
+			test_deploy.InitManifestsSpec()
 		},
 	)
 }
